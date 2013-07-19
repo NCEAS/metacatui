d6d2166b8e6c0d26ef1e8bc1a427243f36a4235d
Christopher Jones <csj@christopherjones.co>
2013-07-18 22:02:50 -0600

    Add in the featured data view of the application, which is the third stripe in the index page, showing scientist's featured data.  This view will need to respond to events originating from that <section>, such as a 'View Dataset' button.

---
+++ metacatui/src/main/webapp/js/views/FeaturedDataView.js
@@ -0,0 +1,24 @@
+/*global define */
+define(['jquery', 'underscore', 'backbone', 'text!templates/featuredData.html'], 				
+	function($, _, Backbone, FeaturedDataTemplate) {
+	'use strict';
+	
+	// Build the featured data view of the application
+	var FeaturedDataView = Backbone.View.extend({
+
+		el: '#FeaturedData',
+		
+		template: _.template(FeaturedDataTemplate),
+		
+		initialize: function () {
+		},
+				
+		render: function () {
+			console.log('Rendering the featured data section');
+			this.$el.html(this.template());
+			
+		}	
+				
+	});
+	return FeaturedDataView;		
+});
