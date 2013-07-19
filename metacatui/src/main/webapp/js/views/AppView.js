d88140616321d0529adf33e2409f535b2666888d
Christopher Jones <csj@christopherjones.co>
2013-07-18 20:26:01 -0600

    Modify the main AppView to be a requires module, and simplify the code such that event responding for sub views like the search view will be handled by that sub view.  This keeps us from having to re-render the entire app when there's an event on just a sub component.  I've deleted a bunch of code here, but it will all be added back into the search sub view.

--- metacatui/src/main/webapp/js/views/AppView.js
+++ metacatui/src/main/webapp/js/views/AppView.js
@@ -1,21 +1,16 @@
 /*global define */
-'use strict';
-
 define(['jquery',
 				'underscore', 
 				'backbone',
-				'collections/SolrResults',
-				'views/SearchResultsView', 
-				'text!templates/statCounts.html',
-				'text!templates/navbar.html',
-				'text!templates/secondaryHeader.html',
-				'text!templates/footer.html'], 				
-	function($, _, Backbone, SolrResultList, SearchResultsView, 
-		statCountsTemplate, mainNavbarTemplate, secondaryHeaderTemplate, mainFooterTemplate) {
-
-	// The Application
-	// ---------------
-
+				'views/NavbarView',
+				'views/MainHeaderView'
+				//'views/features',
+				//'views/featuredData',
+				//'views/footer'
+				], 				
+	function($, _, Backbone, NavbarView, MainHeaderView) {
+	'use strict';
+	
 	// Our overall **AppView** is the top-level piece of UI.
 	var AppView = Backbone.View.extend({
 
@@ -23,183 +18,24 @@ define(['jquery',
 		// the App already present in the HTML.
 		el: '#metacatui-app',
 		
-		statsTemplate: _.template(statCountsTemplate),
-		
-		navbarTemplate: _.template(mainNavbarTemplate),
-
-		headerTemplate: _.template(secondaryHeaderTemplate),
-		
-		footerTemplate: _.template(mainFooterTemplate),
-
-		// Delegated events for creating new items, and clearing completed ones.
-		events: {
-			//'keypress #new-todo': 'createOnEnter',
-			//'click #clear-completed': 'clearCompleted',
-			//'click #toggle-all': 'toggleAllComplete'
-			'click #mostaccessed_link': 'showMostAccessed',
-			'click #results_link': 'showResults',
-			'click #recent_link': 'showRecent',
-			'click #featureddata_link': 'showFeatured',
-			'click #results_prev': 'prevpage',
-			'click #results_next': 'nextpage',
-			'click #search_btn': 'showResults',
-		},
-
-		// At initialization we bind to the relevant events on the `SearchResults`
-		// collection, when items are added or changed.
 		initialize: function () {
-			this.$baseurl = window.location.origin;
-			this.$view_service = this.$baseurl + window.location.pathname + '/d1proxy/view/';
-			this.$resultsview = this.$('#results-view');
-			this.$results = this.$('#results');
-			this.$pagehead = this.$('#pagehead');
-			this.$statcounts = this.$('#statcounts');
-			this.$isCollapsed = false;
-			
-			this.listenTo(app.SearchResults, 'add', this.addOne);
-			this.listenTo(app.SearchResults, 'reset', this.addAll);
-			this.listenTo(app.SearchResults, 'all', this.render);
 
-			app.SearchResults.setfields("id,title,origin,pubDate,dateUploaded,abstract");
-
-			this.render();
 		},
-		
-		// Switch the results view to the most accessed data query
-		showMostAccessed: function () {
-			//this.collapseSlides();
-			this.removeAll();
-			this.$pagehead.html('Most Accessed');
-			app.SearchResults.setrows(10);
-			app.SearchResults.setSort("dateUploaded+asc");
-			app.SearchResults.query("formatType:METADATA+-obsoletedBy:*+most");
-			this.$("#results_link").removeClass("sidebar-item-selected");
-			this.$("#recent_link").removeClass("sidebar-item-selected");
-			this.$("#mostaccessed_link").addClass("sidebar-item-selected");
-			this.$("#featureddata_link").removeClass("sidebar-item-selected");
-			this.$resultsview.show();
-			this.updateStats();
-		},
-		
-		// Switch the results view to the most recent data query 
-		showRecent: function () {
-			//this.collapseSlides();
-			this.removeAll();
-			this.$pagehead.html('Most Recent');
-			app.SearchResults.setrows(10);
-			app.SearchResults.setSort("dateUploaded+desc");
-			app.SearchResults.query("formatType:METADATA+-obsoletedBy:*");
-			this.$("#results_link").removeClass("sidebar-item-selected");
-			this.$("#mostaccessed_link").removeClass("sidebar-item-selected");
-			this.$("#recent_link").addClass("sidebar-item-selected");
-			this.$("#featureddata_link").removeClass("sidebar-item-selected");
-			this.$resultsview.show();
-			this.updateStats();
-		},
-		
-		// Switch the results view to the search results query
-		showResults: function () {
-			this.collapseSlides();
-			var search = this.$("#search_txt").val();
-			this.removeAll();
-			this.$pagehead.html('Search Results');
-			app.SearchResults.setrows(25);
-			app.SearchResults.setSort("title+desc");
-			app.SearchResults.query("formatType:METADATA+-obsoletedBy:*+" + search);
-			this.$("#recent_link").removeClass("sidebar-item-selected");
-			this.$("#mostaccessed_link").removeClass("sidebar-item-selected");
-			this.$("#results_link").addClass("sidebar-item-selected");
-			this.$("#featureddata_link").removeClass("sidebar-item-selected");
-			this.$resultsview.show();
-			this.updateStats();
-		},
-	
-		// Switch the results view to the featured data query
-		showFeatured: function () {
-			this.expandSlides();
-			this.removeAll();
-			this.$pagehead.html('Featured Data');
-			app.SearchResults.setrows(10);
-			app.SearchResults.setSort("dateUploaded+desc");
-			app.SearchResults.query("formatType:METADATA+-obsoletedBy:*+jones");
-			this.$("#recent_link").removeClass("sidebar-item-selected");
-			this.$("#mostaccessed_link").removeClass("sidebar-item-selected");
-			this.$("#results_link").removeClass("sidebar-item-selected");
-			this.$("#featureddata_link").addClass("sidebar-item-selected");
-			this.$resultsview.show();
-			this.updateStats();
-		},
-		
-		updateStats: function () {
-			if (app.SearchResults.header != null) {
-				this.$statcounts.html(this.statsTemplate({
-					start: app.SearchResults.header.get("start")+1,
-					end: app.SearchResults.header.get("start") + app.SearchResults.length,
-					numFound: app.SearchResults.header.get("numFound")
-				}));
-			}
-		},
-		
-		// Next page of results
-		nextpage: function () {
-			this.removeAll();
-			app.SearchResults.nextpage();
-			this.$resultsview.show();
-			this.updateStats();
-		},
-		
-		// Previous page of results
-		prevpage: function () {
-			this.removeAll();
-			app.SearchResults.prevpage();
-			this.$resultsview.show();
-			this.updateStats();
-		},
-		
-		// Re-rendering the App includes refreshing the statistics
+				
+		// Render the main view and/or re-render subviews. Don't call .html() here
+		// so we don't lose state, rather use .setElement(). Delegate rendering 
+		// and event handling to sub views
 		render: function () {
-			if (app.SearchResults.length) {
-				this.$resultsview.show();
-				this.updateStats();
-			} else {
-				this.$resultsview.hide();
-			}
-		},
-		
-		// Add a single SolrResult item to the list by creating a view for it, and
-		// appending its element to the `<ul>`.
-		addOne: function (result) {
-			result.set( {baseurl: this.$view_service} );
-			var view = new app.SearchResultView({ model: result });
-			this.$results.append(view.render().el);
-		},
-
-		// Add all items in the **SearchResults** collection at once.
-		addAll: function () {
-			app.SearchResults.each(this.addOne, this);
-		},
-		
-		// Remove all html for items in the **SearchResults** collection at once.
-		removeAll: function () {
-			this.$results.html('');
-		},
-		
-		// Collapse the top slide carousel to display full page
-		collapseSlides: function () {
-			this.$("#main_body").addClass("main-body-padded");
-			$('#mainPageSlides').collapse("hide");
-			this.$isCollapsed = true;
-		},
-		
-		// Expand the top slide carousel to display partial page
-		expandSlides: function () {
-			if (this.$isCollapsed) {
-				this.$("#main_body").removeClass("main-body-padded");
-				$('#mainPageSlides').collapse("show");
-				this.$isCollapsed = false;
-			}
-		}
-		
+			console.log('Rendering subviews here.');
+			this.navbarView = new NavbarView();
+			this.navbarView.setElement(this.$('#Navbar')).render();
+			
+			this.mainHeaderView = new MainHeaderView();
+			this.mainHeaderView.setElement(this.$('#mainHeader')).render();
+			
+			return this;
+		}	
+				
 	});
 	return AppView;		
 });
