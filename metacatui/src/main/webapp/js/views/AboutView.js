82abfd9e1c8f483ed2b13ff9c6976f6be5d8b576
Christopher Jones <csj@christopherjones.co>
2013-07-18 20:33:14 -0600

    Add in the AboutView view.

---
+++ metacatui/src/main/webapp/js/views/AboutView.js
@@ -0,0 +1,24 @@
+/*global define */
+define(['jquery', 'underscore', 'backbone', 'text!templates/about.html'], 				
+	function($, _, Backbone, AboutTemplate) {
+	'use strict';
+	
+	// Build the main header view of the application
+	var AboutView = Backbone.View.extend({
+
+		el: '#mainHeader',
+		
+		template: _.template(AboutTemplate),
+		
+		initialize: function () {
+		},
+				
+		render: function () {
+			console.log('Rendering the about view');
+			this.$el.html(this.template());
+			
+		}	
+				
+	});
+	return AboutView;		
+});
