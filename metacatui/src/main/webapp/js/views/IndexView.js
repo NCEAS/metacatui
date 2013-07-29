175ef0245362a31e51a7387c5da172aae283ee3f
Christopher Jones <csj@christopherjones.co>
2013-07-29 11:16:02 -0600

    Ensure subviews get postRender() called.  Also, add in the call to run the carousel.  Still working on the prev/next button functionality and layout.

--- metacatui/src/main/webapp/js/views/IndexView.js
+++ metacatui/src/main/webapp/js/views/IndexView.js
@@ -2,10 +2,11 @@
 define(['jquery',
 				'underscore', 
 				'backbone',
+				'bootstrap',
 				'views/FeaturesView',
 				'views/FeaturedDataView'
 				], 				
-	function($, _, Backbone, FeaturesView, FeaturedDataView) {
+	function($, _, Backbone, Bootstrap, FeaturesView, FeaturedDataView) {
 	'use strict';
 	
 	var app = app || {};
@@ -27,15 +28,24 @@ define(['jquery',
 			
 			appModel.set('headerType', 'main');
 			appModel.set('navbarPosition', 'absolute');		
-				
+			
+			// enable the Catousel
+			$('.carousel').carousel();
+			
 			// Add in the Features section
 			this.$el.html('<section id="Features" /><section id="FeaturedData" />');			
 			var featuresView = new FeaturesView();
 			featuresView.setElement($('#Features')).render();
+			if ( featuresView.postRender ) {
+				featuresView.postRender();
+			}
 			
 			// Add in the FeaturedData section
-			this.featuredDataView = new FeaturedDataView();
-			this.featuredDataView.setElement(this.$('#FeaturedData')).render();
+			var featuredDataView = new FeaturedDataView();
+			featuredDataView.setElement(this.$('#FeaturedData')).render();
+			if ( featuredDataView.postRender ) {
+				featuredDataView.postRender();
+			}
 			
 			return this;
 		}	
