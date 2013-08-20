704d8985d404dc46e9f77a6017614799916486b2
leinfelder <leinfelder@nceas.ucsb.edu>
2013-08-20 11:23:09 -0700

    use "custom" navbar and footer for the KNB skin. (just copied from the default)

--- metacatui/src/main/webapp/themes/knb/config.js
+++ metacatui/src/main/webapp/themes/knb/config.js
@@ -2,9 +2,11 @@ var skin = skin || "knb";
 var skinMap = 
 {
 	'*': {
-		// example overrides are provided here
+		// overrides are provided here
 		//'models/AboutModel' : '/themes/' + skin + '/models/AboutModel.js',
 		//'views/AboutView' : '/themes/' + skin + '/views/AboutView.js',
-		//'templates/navbar.html' : '/themes/' + skin + '/templates/navbar.html'
+		'routers/router' : '/themes/' + skin + '/routers/router.js',
+		'templates/navbar.html' : '/themes/' + skin + '/templates/navbar.html',
+		'templates/footer.html' : '/themes/' + skin + '/templates/footer.html'
 		}
 };
