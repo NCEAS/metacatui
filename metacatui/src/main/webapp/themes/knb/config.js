9e7b439ab401ca9316e6b6154067dcb866f95a94
leinfelder <leinfelder@nceas.ucsb.edu>
2013-08-20 10:45:55 -0700

    include the correct favicon and title based on the chosen theme. include start for the KNB theme (basically everything in the default theme will need to be moved into knb as an "override", but we want the core to be in default where it can be re-used).

---
+++ metacatui/src/main/webapp/themes/knb/config.js
@@ -0,0 +1,10 @@
+var skin = skin || "knb";
+var skinMap = 
+{
+	'*': {
+		// example overrides are provided here
+		//'models/AboutModel' : '/themes/' + skin + '/models/AboutModel.js',
+		//'views/AboutView' : '/themes/' + skin + '/views/AboutView.js',
+		//'templates/navbar.html' : '/themes/' + skin + '/templates/navbar.html'
+		}
+};
