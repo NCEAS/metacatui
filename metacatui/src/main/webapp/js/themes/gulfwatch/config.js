4f57bd613c2ee141b9af62e6bf569b09f30d4331
Lauren <walker@nceas.ucsb.edu>
2013-08-21 09:34:55 -0700

    Added Gulf Watch theme css and images and templates

--- metacatui/src/main/webapp/js/themes/gulfwatch/config.js
+++ metacatui/src/main/webapp/js/themes/gulfwatch/config.js
@@ -5,6 +5,9 @@ var themeMap =
 		// example overrides are provided here
 		//'models/AboutModel' : 'themes/' + theme + '/models/AboutModel.js',
 		//'views/AboutView' : 'themes/' + theme + '/views/AboutView.js',
-		'templates/navbar.html' : 'themes/' + theme + '/templates/navbar.html'
+		'templates/navbar.html' : 'themes/' + theme '/templates/navbar.html',
+		'templates/footer.html' : 'themes/' + theme + '/templates/footer.html',
+		'templates/search.html' : 'themes/' + theme + '/templates/search.html',
+		'templates/defaultHeader.html' : 'themes/' + theme + '/templates/defaultHeader.html'
 		}
 };
