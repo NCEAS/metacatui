a63754df977d5d8e56650a56668a1ad63976cf3e
leinfelder <leinfelder@nceas.ucsb.edu>
2013-11-08 11:35:52 -0800

    testing dup 1.2 branch commit with GOA changes

--- metacatui/src/main/webapp/js/themes/goa/routers/router.js
+++ metacatui/src/main/webapp/js/themes/goa/routers/router.js
@@ -12,7 +12,10 @@ function ($, _, Backbone, IndexView, AboutView, ToolsView, DataCatalogView, Regi
 	var metadataView = new MetadataView();
 	var externalView = new ExternalView();
 	var ldapView = new LdapView();
-
+	
+	// set the KNB as the only LDAP servicer for this theme
+	// NOTE: requires CORS configured on the web server
+	appModel.set('ldapwebServiceUrl', 'https://knb.ecoinformatics.org/knb/cgi-bin/ldapweb.cgi');
 	
 	// MetacatUI Router
 	// ----------------
