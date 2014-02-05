18ad135d53acc86a57b34f4071797109242dc119
Lauren <walker@nceas.ucsb.edu>
2014-02-04 17:01:56 -0800

    Profile View can handle queries that return 0 metadata or 0 data objects without failing.

--- metacatui/src/main/webapp/js/views/ProfileView.js
+++ metacatui/src/main/webapp/js/views/ProfileView.js
@@ -1,6 +1,6 @@
 /*global define */
-define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text!templates/alert.html'], 				
-	function($, _, Backbone, ProfileTemplate, AlertTemplate) {
+define(['jquery', 'underscore', 'backbone', 'd3', 'text!templates/profile.html', 'text!templates/alert.html'], 				
+	function($, _, Backbone, d3, ProfileTemplate, AlertTemplate) {
 	'use strict';
 		
 	// Build the main header view of the application
@@ -17,6 +17,8 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 				
 		render: function () {
 			
+			this.$el.html(this.template());
+			
 			// set the header type
 			appModel.set('headerType', 'default');
 			
@@ -31,6 +33,7 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 			}
 			
 			this.getFormatTypes(query);
+
 			
 			return this;
 		},
@@ -39,7 +42,7 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 			console.log('Closing the profile view');
 		},
 		
-		postRender: function() {
+		postRender: function(){
 
 		},
 		
@@ -48,7 +51,7 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 			
 			//Build the query to get the format types
 			var facetFormatType = "q=" + query +
-								  "+(formatType:METADATA%20OR%20formatType:DATA)" +
+								  "+%28formatType:METADATA%20OR%20formatType:DATA%29" +
 								  "&wt=json" +
 								  "&rows=2" +
 							 	  "&group=true" +
@@ -59,32 +62,37 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 			//Run the query
 			$.get(appModel.get('queryServiceUrl') + facetFormatType, function(data, textStatus, xhr) {
 				
-				//Extract the format types (because of filtering and sorting they will always return in this order)
-				viewRef.metadataCount = data.grouped.formatType.groups[0].doclist.numFound;
-				viewRef.dataCount = data.grouped.formatType.groups[1].doclist.numFound;
-				
+				if(data.grouped.formatType.groups.length == 1){
+					
+					//Extract the format type if there is only one type found
+					if(data.grouped.formatType.groups[0].groupValue == "METADATA"){
+						viewRef.metadataCount = data.grouped.formatType.groups[0].doclist.numFound;
+					}else{
+						viewRef.dataCount = data.grouped.formatType.groups[0].doclist.numFound;
+					}					
+				}	
 				//If no data or metadata objects were found, display a warning
-				if((viewRef.dataCount==0) && (viewRef.metadataCount==0)){
+				else if(data.grouped.formatType.groups.length == 0){
 					console.warn('No metadata or data objects found. Stopping view load.');
-					var msg = "No metadata or data objects were found for that criteria. Please try again.";
-					viewRef.$el.html(viewRef.alertTemplate({
+					var msg = "No data sets were found for that criteria.";
+					viewRef.$el.prepend(viewRef.alertTemplate({
 						msg: msg,
-						classes: "alert-block"
+						classes: "alert-error"
 					}));
 				}
 				else{
-						var dataFormatIds = "q=formatType:DATA" +
-											"&facet=true" +
-											"&facet.field=formatId" +
-											"&facet.limit=-1" +
-											"&wt=json" +
-											"&rows=0";
-					var metadataFormatIds = "q=formatType:METADATA" +
-											"&facet=true" +
-											"&facet.field=formatId" +
-											"&facet.limit=-1" +
-											"&wt=json" +
-											"&rows=0";
+					//Extract the format types (because of filtering and sorting they will always return in this order)
+					viewRef.metadataCount = data.grouped.formatType.groups[0].doclist.numFound;
+					viewRef.dataCount = data.grouped.formatType.groups[1].doclist.numFound;
+				}	
+
+				if(viewRef.dataCount > 0){
+					var dataFormatIds = "q=formatType:DATA" +
+					"&facet=true" +
+					"&facet.field=formatId" +
+					"&facet.limit=-1" +
+					"&wt=json" +
+					"&rows=0";
 					
 					//Now get facet counts of the data format ID's 
 					$.get(appModel.get('queryServiceUrl') + dataFormatIds, function(data, textStatus, xhr) {
@@ -92,6 +100,15 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 					}).error(function(){
 						console.warn('Solr query error for data formatIds - not vital to page, so we will keep going');
 					});
+				}
+				
+				if(viewRef.metadataCount > 0){
+					var metadataFormatIds = "q=formatType:METADATA" +
+					"&facet=true" +
+					"&facet.field=formatId" +
+					"&facet.limit=-1" +
+					"&wt=json" +
+					"&rows=0";
 					
 					//Now get facet counts of the metadata format ID's 
 					$.get(appModel.get('queryServiceUrl') + metadataFormatIds, function(data, textStatus, xhr) {
@@ -99,18 +116,17 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/profile.html', 'text
 					}).error(function(){
 						console.warn('Solr query error for metadata formatIds - not vital to page, so we will keep going');
 					});
-				
-					viewRef.$el.html(viewRef.template({
-						dataCount: viewRef.dataCount,
-						metadataCount: viewRef.metadataCount
-					}));
-				
-				}//end else
+				}
+					
+				//Insert the counts into the DOM
+				$('#data-chart').html(viewRef.dataCount);
+				$('#metadata-chart').html(viewRef.metadataCount);
+						
 			//Display error when our original Solr query went wrong
 			}).error(function(){
 				console.error('Solr query returned error, stopping view load');
 				var msg = "It seems there has been an error. Please try again.";
-				viewRef.$el.html(viewRef.alertTemplate({
+				viewRef.$el.prepend(viewRef.alertTemplate({
 					msg: msg,
 					classes: "alert-error"
 				}));
