25d5a25fe312801c7ef1dc9ee442af219f9023f1
leinfelder <leinfelder@nceas.ucsb.edu>
2014-03-25 14:59:15 -0700

    Include handling for standard_sm and characteristic_sm index fields but only expose them in the new "semtools" theme. https://projects.ecoinformatics.org/ecoinfo/issues/6254

---
+++ metacatui/src/main/webapp/js/themes/semtools/config.js
@@ -0,0 +1,14 @@
+var theme = theme || "semtools";
+var themeMap = 
+{
+	'*': {
+		// Template overrides are provided here
+		
+		// Resources (js) omit extension
+		//'views/AboutView' : 'themes/' + theme + '/views/AboutView',
+		//'routers/router' : 'themes/' + theme + '/routers/router',
+		
+		// Templates include extension
+		'templates/search.html' : 'themes/' + theme + '/templates/search.html'
+		}
+};
--- metacatui/src/main/webapp/js/views/DataCatalogView.js
+++ metacatui/src/main/webapp/js/views/DataCatalogView.js
@@ -428,6 +428,40 @@ define(['jquery',
 				
 			}
 			
+			// characteristic
+			var thisCharacteristic = null;
+			var characteristic = searchModel.get('characteristic');
+			
+			for (var i=0; i < characteristic.length; i++){
+				
+				//Trim the spaces off
+				thisCharacteristic = characteristic[i].trim();
+				
+				// encode the semantic URI
+				thisCharacteristic = "%22" + encodeURIComponent(thisCharacteristic) + "%22";
+				
+				// add to the query
+				filterQuery += "&fq=characteristic_sm:" + thisCharacteristic;
+				
+			}
+			
+			// standard
+			var thisStandard = null;
+			var standard = searchModel.get('standard');
+			
+			for (var i=0; i < standard.length; i++){
+				
+				//Trim the spaces off
+				thisStandard = standard[i].trim();
+				
+				// encode the semantic URI
+				thisStandard = "%22" + encodeURIComponent(thisStandard) + "%22";
+				
+				// add to the query
+				filterQuery += "&fq=standard_sm:" + thisStandard;
+				
+			}
+			
 			//All
 			var thisAll = null;
 			var all = searchModel.get('all');
@@ -922,8 +956,13 @@ define(['jquery',
 			}
 			
 							
+			// is it a semantic concept?
+			var termLabel = null;
+			if (term.indexOf("#") > 0) {
+				termLabel = term.substring(term.indexOf("#"));
+			}
 			//Add a filter node to the DOM
-			e.prepend(viewRef.currentFilterTemplate({filterTerm: term}));	
+			e.prepend(viewRef.currentFilterTemplate({filterTerm: term, termLabel: termLabel}));	
 				
 			return;
 		},
@@ -1140,6 +1179,8 @@ define(['jquery',
 							 "&facet.field=attributeName" +
 							 "&facet.field=attributeLabel" +
 							 "&facet.field=site" +
+							 "&facet.field=characteristic_sm" +
+							 "&facet.field=standard_sm" +
 							 "&facet.mincount=1" +
 							 "&facet.limit=-1";
 
@@ -1218,6 +1259,76 @@ define(['jquery',
 					select: function(event, ui) {
 						// set the text field
 						$('#attribute_input').val(ui.item.value);
+						// add to the filter immediately
+						viewRef.updateTextFilters(event);
+						// prevent default action
+						return false;
+					},
+					position: {
+						my: "left top",
+						at: "left bottom"				
+					}
+				});
+				
+				// suggest characteristics
+				var characteristicSuggestions = facetCounts.characteristic_sm;
+				var rankedCharacteristicSuggestions = new Array();
+				for (var i=0; i < characteristicSuggestions.length-1; i+=2) {
+					rankedCharacteristicSuggestions.push({value: characteristicSuggestions[i], label: characteristicSuggestions[i].substring(characteristicSuggestions[i].indexOf("#")) });
+				}
+				$('#characteristic_input').autocomplete({
+					source: function (request, response) {
+			            var term = $.ui.autocomplete.escapeRegex(request.term)
+			                , startsWithMatcher = new RegExp("^" + term, "i")
+			                , startsWith = $.grep(rankedCharacteristicSuggestions, function(value) {
+			                    return startsWithMatcher.test(value.label || value.value || value);
+			                })
+			                , containsMatcher = new RegExp(term, "i")
+			                , contains = $.grep(rankedCharacteristicSuggestions, function (value) {
+			                    return $.inArray(value, startsWith) < 0 && 
+			                        containsMatcher.test(value.label || value.value || value);
+			                });
+			            
+			            response(startsWith.concat(contains));
+			        },
+					select: function(event, ui) {
+						// set the text field
+						$('#characteristic_input').val(ui.item.value);
+						// add to the filter immediately
+						viewRef.updateTextFilters(event);
+						// prevent default action
+						return false;
+					},
+					position: {
+						my: "left top",
+						at: "left bottom"				
+					}
+				});
+				
+				// suggest standards
+				var standardSuggestions = facetCounts.standard_sm;
+				var rankedStandardSuggestions = new Array();
+				for (var i=0; i < standardSuggestions.length-1; i+=2) {
+					rankedStandardSuggestions.push({value: standardSuggestions[i], label: standardSuggestions[i].substring(standardSuggestions[i].indexOf("#")) });
+				}
+				$('#standard_input').autocomplete({
+					source: function (request, response) {
+			            var term = $.ui.autocomplete.escapeRegex(request.term)
+			                , startsWithMatcher = new RegExp("^" + term, "i")
+			                , startsWith = $.grep(rankedStandardSuggestions, function(value) {
+			                    return startsWithMatcher.test(value.label || value.value || value);
+			                })
+			                , containsMatcher = new RegExp(term, "i")
+			                , contains = $.grep(rankedStandardSuggestions, function (value) {
+			                    return $.inArray(value, startsWith) < 0 && 
+			                        containsMatcher.test(value.label || value.value || value);
+			                });
+			            
+			            response(startsWith.concat(contains));
+			        },
+					select: function(event, ui) {
+						// set the text field
+						$('#standard_input').val(ui.item.value);
 						// add to the filter immediately
 						viewRef.updateTextFilters(event);
 						// prevent default action
