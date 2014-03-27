0768dfaae086f336d857aefe82af0214f2cf5bee
Lauren <walker@nceas.ucsb.edu>
2014-03-27 15:45:52 -0700

    Added a d1 theme for demo purposes. Only works with the MN API for now.

---
+++ metacatui/src/main/webapp/js/themes/dataone/config.js
@@ -0,0 +1,12 @@
+var theme = theme || "default";
+var themeMap = 
+{
+	'*': {
+		// example overrides are provided here
+		//'views/AboutView' : 'themes/' + theme + '/views/AboutView.js',
+		'templates/navbar.html' : 'themes/' + theme + '/templates/navbar.html',
+		'templates/footer.html' : 'themes/' + theme + '/templates/footer.html',
+		'templates/about.html' : 'themes/' + theme + '/templates/about.html',
+		'views/DataCatalogView' : 'js/themes/' + theme + '/views/DataCatalogView.js'
+		}
+};
