ff733e35f9bd80ed83d21a3cdbee1195e405993a
Lauren <walker@nceas.ucsb.edu>
2014-04-15 14:49:14 -0700

    Add a list of NCEAS working groups keywords to the NCEAS theme Registry.

--- metacatui/src/main/webapp/js/themes/nceas/config.js
+++ metacatui/src/main/webapp/js/themes/nceas/config.js
@@ -4,10 +4,12 @@ var themeMap =
 	'*': {
 		// example overrides are provided here
 		//'views/AboutView' : 'themes/' + theme + '/views/AboutView.js',
-		'templates/navbar.html' 	 : 'themes/' + theme + '/templates/navbar.html',
-		'templates/app.html' 		 : 'themes/' + theme + '/templates/app.html',
-		'templates/footer.html' 	 : 'themes/' + theme + '/templates/footer.html',
-		'templates/loginHeader.html' : 'themes/' + theme + '/templates/loginHeader.html',
-		'routers/router' 			 : 'js/themes/' + theme + '/routers/router.js'
+		'templates/navbar.html' 	 	: 'themes/' + theme + '/templates/navbar.html',
+		'templates/app.html' 		 	: 'themes/' + theme + '/templates/app.html',
+		'templates/footer.html' 	 	: 'themes/' + theme + '/templates/footer.html',
+		'templates/loginHeader.html' 	: 'themes/' + theme + '/templates/loginHeader.html',
+		'templates/registryFields.html' : 'themes/' + theme + '/templates/registryFields.html',
+		'models/RegistryModel'	     	: 'js/themes/' + theme + '/models/RegistryModel.js',
+		'routers/router' 			 	: 'js/themes/' + theme + '/routers/router.js'
 		}
 };
