b8465de320769499848a9192b3c42cd9cde5342c
Lauren <walker@nceas.ucsb.edu>
2015-02-17 09:44:33 -0800

    Fix big where the rightsHolder query for "My Packages" had a misplaced "+" character

--- metacatui/src/main/webapp/js/views/NavbarView.js
+++ metacatui/src/main/webapp/js/views/NavbarView.js
@@ -86,7 +86,7 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/navbar.html'],
 			searchModel.clear().set(searchModel.defaults);
 			
 			// Create a new array with the new search term
-			var newSearch = ["+rightsHolder:" + username];
+			var newSearch = ["rightsHolder:" + username];
 			
 			//Set up the search model for this new term
 			searchModel.set('additionalCriteria', newSearch);
