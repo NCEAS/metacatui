e1e6685720be44402369b3260881747e91afab52
Lauren <walker@nceas.ucsb.edu>
2015-05-20 09:53:02 -0700

    Update the NCEAS theme introduction text and use a custom AppModel to point to KNB

--- metacatui/src/main/webapp/js/themes/nceas/config.js
+++ metacatui/src/main/webapp/js/themes/nceas/config.js
@@ -10,6 +10,7 @@ var themeMap =
 		'templates/loginHeader.html' 	: 'themes/' + theme + '/templates/loginHeader.html',
 		'templates/registryFields.html' : 'themes/' + theme + '/templates/registryFields.html',
 		'models/RegistryModel'	     	: 'js/themes/' + theme + '/models/RegistryModel.js',
+		'models/AppModel'	     	    : 'js/themes/' + theme + '/models/AppModel.js',
 		'routers/router' 			 	: 'js/themes/' + theme + '/routers/router.js'
 		}
 };
---
+++ metacatui/src/main/webapp/js/themes/nceas/models/AppModel.js
@@ -0,0 +1,65 @@
+/*global define */
+define(['jquery', 'underscore', 'backbone'], 				
+	function($, _, Backbone) {
+	'use strict';
+
+	// Application Model
+	// ------------------
+	var AppModel = Backbone.Model.extend({
+		// This model contains all of the attributes for the Application
+		defaults: {
+			headerType: 'default',
+			searchMode: 'map',
+			username: null,
+			fullName: null,
+			sortOrder: 'dateUploaded+desc',
+			pid: null,
+			anchorId: null,
+			page: null,
+			profileQuery: null,
+			baseUrl: "https://knb.ecoinformatics.org",
+			// the most likely item to change is the Metacat deployment context
+			context: '/metacat',
+			d1Service: '/d1/mn/v1',
+			viewServiceUrl: null,
+			packageServiceUrl: null,
+			publishServiceUrl: null,
+			authServiceUrl: null,
+			queryServiceUrl: null,
+			metaServiceUrl: null,
+			registryServiceUrl: null,
+			ldapwebServiceUrl: null,
+			metacatServiceUrl: null,
+			objectServiceUrl: null,
+			bioportalServiceUrl: null
+		},
+				
+		defaultView: "share",
+		
+		initialize: function() {
+			
+			//For IE
+			if (!window.location.origin) {
+				var baseUrl = window.location.protocol + "//" + window.location.host;
+				
+				this.set('baseUrl', baseUrl);
+			}
+			
+			// these are pretty standard, but can be customized if needed
+			this.set('viewServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/views/metacatui/');
+			this.set('packageServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/package/');
+			this.set('publishServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/publish/');
+			this.set('authServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/isAuthorized/');
+			this.set('queryServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/query/solr/');
+			this.set('metaServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/meta/');
+			this.set('objectServiceUrl', this.get('baseUrl') + this.get('context') + this.get('d1Service') + '/object/');
+			this.set('registryServiceUrl', this.get('baseUrl') + this.get('context') + '/cgi-bin/register-dataset.cgi');
+			this.set('ldapwebServiceUrl', this.get('baseUrl') + this.get('context') + '/cgi-bin/ldapweb.cgi');
+			this.set('metacatServiceUrl', this.get('baseUrl') + this.get('context') + '/metacat');
+			
+		}
+	
+		
+	});
+	return AppModel;		
+});
