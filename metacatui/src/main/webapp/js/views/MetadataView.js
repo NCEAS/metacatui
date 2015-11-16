7d119f17bbed726cd77483c8793c0eb273a7daf4
Lauren <walker@nceas.ucsb.edu>
2015-11-16 15:26:35 -0800

    Forgot zoom level change

--- metacatui/src/main/webapp/js/views/MetadataView.js
+++ metacatui/src/main/webapp/js/views/MetadataView.js
@@ -588,7 +588,7 @@ define(['jquery',
 				var url = "https://maps.google.com/?ll=" + latLngCEN.lat() + "," + latLngCEN.lng() + 
 						  "&spn=0.003833,0.010568" +
 						  "&t=m" +
-						  "&z=16";
+						  "&z=14";
 				//Create a google map image
 				var mapHTML = "<img class='georegion-map' " +
 							  "src='https://maps.googleapis.com/maps/api/staticmap?" +
