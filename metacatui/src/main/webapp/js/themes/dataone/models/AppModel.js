439aecfdaf06cf1625326e32ca59bf98585855a0
Lauren <walker@nceas.ucsb.edu>
2015-12-14 12:11:47 -0800

    Use the base URL for the resolve service in DataONE theme to use proxy server - that way Firefox will support the download attribute (FF only supports same-origin downlaod links)

--- metacatui/src/main/webapp/js/themes/dataone/models/AppModel.js
+++ metacatui/src/main/webapp/js/themes/dataone/models/AppModel.js
@@ -83,7 +83,7 @@ define(['jquery', 'underscore', 'backbone'],
 			this.set('authServiceUrl',    this.get('baseUrl')  + this.get('d1Service') + '/isAuthorized/');
 			this.set('queryServiceUrl',   this.get('baseUrl')  + this.get('d1Service') + '/query/solr/');
 			this.set('metaServiceUrl',    this.get('baseUrl')  + this.get('d1Service') + '/meta/');
-			this.set('resolveServiceUrl', this.get('d1CNBaseUrl')  + this.get('d1Service') + '/resolve/');
+			this.set('resolveServiceUrl', this.get('baseUrl')  + this.get('d1Service') + '/resolve/');
 			this.set('nodeServiceUrl',    this.get('baseUrl')  + this.get('d1Service') + '/node');
 			
 			this.set('d1LogServiceUrl',   this.get('baseUrl') + this.get('d1Service') + '/query/logsolr/');
