479985e40c668ed8a17c756372f3053b52e3c9ef
Lauren <walker@nceas.ucsb.edu>
2016-03-03 11:43:42 -0800

    Show user settings in Arctica theme

--- metacatui/src/main/webapp/js/themes/arctica/config.js
+++ metacatui/src/main/webapp/js/themes/arctica/config.js
@@ -13,6 +13,7 @@ var themeMap =
 		'templates/defaultHeader.html' : 'themes/' + theme + '/templates/defaultHeader.html',
 		'templates/tools.html' : 'themes/' + theme + '/templates/tools.html',
 		'templates/about.html' : 'themes/' + theme + '/templates/about.html',
+		'templates/userProfileMenu.html' : 'themes/' + theme + '/templates/userProfileMenu.html',
 		'models/AppModel' : 'js/themes/' + theme + '/models/AppModel.js'
 		}
 };
