95acf287cc52ff193b355bc342c6f62e67f569cf
Lauren <walker@nceas.ucsb.edu>
2016-05-11 11:00:04 -0700

    Trim whitespace from filenames when downloading

--- metacatui/src/main/webapp/js/models/SolrResult.js
+++ metacatui/src/main/webapp/js/models/SolrResult.js
@@ -217,7 +217,7 @@ define(['jquery', 'underscore', 'backbone'],
 			xhr.onload = function(){ 
 			    var a = document.createElement('a');
 			    a.href = window.URL.createObjectURL(xhr.response); // xhr.response is a blob
-			    a.download = filename; // Set the file name.
+			    a.download = filename.trim(); // Set the file name.
 			    a.style.display = 'none';
 			    document.body.appendChild(a);
 			    a.click();
