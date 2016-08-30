affb334a70d0fc9a16f951ca0847382c370c3f44
walker <walker@128.111.220.53>
2016-08-30 10:28:36 -0700

    Prepare for 1.12.0 tag

--- metacatui/src/main/webapp/js/themes/arctic/models/AppModel.js
+++ metacatui/src/main/webapp/js/themes/arctic/models/AppModel.js
@@ -34,7 +34,7 @@ define(['jquery', 'underscore', 'backbone'],
 			
 			maxDownloadSize: 3000000000,
 			
-			metacatVersion: "2.7.1", 
+			metacatVersion: "2.7.2", 
 			baseUrl: window.location.origin || (window.location.protocol + "//" + window.location.host),
 			// the most likely item to change is the Metacat deployment context
 			context: '/metacat',
