a5c219bd386c342f9e8fd5ab3f330fd6230d9e1e
Christopher Jones <csj@christopherjones.co>
2016-09-28 17:31:35 -0600

    Stub out the EditorView, which is a container view for the DataPackageView and the EML211View, both UI components of the editor.

---
+++ metacatui/src/main/webapp/js/views/EditorView.js
@@ -0,0 +1,58 @@
+﻿/* global define */
+define(['underscore', 'jquery', 'backbone', 'text!templates/editor.html'], function(_, $, Backbone, EditorTemplate){
+    
+    var EditorView = Backbone.View.extend({
+        
+        el: '#Content',
+        
+        /* The initial editor layout */
+        template: _.template(EditorTemplate),
+        
+        /* Events that apply to the entire editor */
+        events: {
+            
+        },
+        
+        /* The identifier of the root package id being rendered */
+        id: null,
+        
+        /* A list of the subviews of the editor */
+        subviews: [],
+        
+        /* Constructor - initialize a new EditorView */
+        initialize: function(options) {
+            
+            // If options.id isn't present, generate and render a new package id and metadata id
+            if ( typeof options === "undefined" || options.id === null ) {
+                console.log("EditorView: Creating a new data package.");
+                
+            } else {
+                this.id = options.id;
+                console.log("Loading existing package from id " + options.id);
+                
+            }            
+            return this;
+        },
+        
+        /* Render the view */
+        render: function() {
+            return this;
+        },
+        /* Close the view and its sub views */
+        close: function() {
+            this.remove(); // remove for the DOM, stop listening           
+            this.off();    // remove callbacks, prevent zombies         
+            
+            // Close each subview
+            _.each(this.subviews, function(i, subview) {
+				subview.close();
+                
+            });
+            
+            this.subviews = [];
+			window.onbeforeunload = null;
+            
+        }
+    });
+    return EditorView;
+});
