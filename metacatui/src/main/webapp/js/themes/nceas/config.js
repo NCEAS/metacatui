db1bc8b27bb9db70faf654ab30b4605fc9ef9c28
Christopher Jones <csj@christopherjones.co>
2016-09-28 17:32:53 -0600

    Remove RegistryModel references from the NCEAS theme config as we extract it from the app.

--- metacatui/src/main/webapp/js/themes/nceas/config.js
+++ metacatui/src/main/webapp/js/themes/nceas/config.js
@@ -9,9 +9,7 @@ MetacatUI.themeMap =
 		'templates/app.html' 		 	: 'themes/' + MetacatUI.theme + '/templates/app.html',
 		'templates/footer.html' 	 	: 'themes/' + MetacatUI.theme + '/templates/footer.html',
 		'templates/loginHeader.html' 	: 'themes/' + MetacatUI.theme + '/templates/loginHeader.html',
-		'templates/registryFields.html' : 'themes/' + MetacatUI.theme + '/templates/registryFields.html',
 		'models/AppModel'	     	    : 'js/themes/' + MetacatUI.theme + '/models/AppModel.js',
-		'models/RegistryModel'	     	: 'js/themes/' + MetacatUI.theme + '/models/RegistryModel.js',
 		'routers/router' 			 	: 'js/themes/' + MetacatUI.theme + '/routers/router.js'
 		}
 };
