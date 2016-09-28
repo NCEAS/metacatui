fbbb0f647f003196ddc633cc7238489cfd818cd6
Christopher Jones <csj@christopherjones.co>
2016-09-28 17:29:37 -0600

    Stub out the DataPackageView.

---
+++ metacatui/src/main/webapp/js/views/DataPackageView.js
@@ -0,0 +1,34 @@
+﻿/* global define */
+define([
+    'jquery',
+    'underscore',
+    'backbone',
+    'models/DataPackage',
+    'models/DataONEObject',
+    'models/metadata/ScienceMetadata',
+    'models/metadata/eml211/EML211',
+    'views/EML211View',
+    'models/UserModel'], 
+    function($, _, Backbone, DataPackage, DataONEObject, ScienceMetadata, EML211, EML211View, UserModel) {
+        'use strict';
+        
+        /*
+            The main view of a Data Package in the editor.  The view is
+            composed of a citation section at the top, a file/folder browser
+            below it, followed by a metadata viewer below that, which has a nav
+            sidebar, and a metadata content section.
+        */
+        var DataPackageView = Backbone.View.extend({
+            
+            el: "#Content",
+            
+            dataPackage: null,
+            
+            initialize: function(options) {
+                
+            },
+            
+            
+        });
+        return DataPackageView;
+});
