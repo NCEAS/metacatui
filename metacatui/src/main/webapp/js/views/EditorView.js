5015ebcb8735f9d4e697fe63c0e8a3f117838cf1
Christopher Jones <csj@christopherjones.co>
2016-09-28 17:56:08 -0600

    Inject the editor template into the #Content div.

--- metacatui/src/main/webapp/js/views/EditorView.js
+++ metacatui/src/main/webapp/js/views/EditorView.js
@@ -36,6 +36,8 @@ define(['underscore', 'jquery', 'backbone', 'text!templates/editor.html'], funct
         
         /* Render the view */
         render: function() {
+			this.$el.append(this.template());
+            
             return this;
         },
         /* Close the view and its sub views */
