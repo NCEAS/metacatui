52b2c36ad13cff459d865c72424b7260450e5eda
leinfelder <leinfelder@nceas.ucsb.edu>
2016-09-29 16:19:19 -0700

    only use the first person from the accounts lookup when showing annotation details.

--- metacatui/src/main/webapp/js/models/AnnotationModel.js
+++ metacatui/src/main/webapp/js/models/AnnotationModel.js
@@ -43,8 +43,8 @@ define(['jquery', 'underscore', 'backbone'],
 				url: appModel.get("accountsUrl") + encodeURIComponent(username),
 				type: "GET",
 				success: function(data, textStatus, xhr){
-					var lastName = $(data).find("person").find("familyName").text(),
-						firstName = $(data).find("person").find("givenName").text();
+					var lastName = $(data).find("person").first().find("familyName").text(),
+						firstName = $(data).find("person").first().find("givenName").text();
 					
 					model.set("name", firstName + " " + lastName);
 					
