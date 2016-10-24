16bef2c83e1f8d6c99a8988bcf234fe6704e5315
Christopher Jones <csj@christopherjones.co>
2016-10-24 10:55:42 -0600

    Build the data package view table, and listen to additions to the collection.  Add the addOne() and addAll() methods for responding to collection changes, adding rows to the table.

--- metacatui/src/main/webapp/js/views/DataPackageView.js
+++ metacatui/src/main/webapp/js/views/DataPackageView.js
@@ -3,30 +3,81 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'models/DataPackage',
+    'collections/DataPackage',
     'models/DataONEObject',
     'models/metadata/ScienceMetadata',
-    'models/metadata/eml211/EML211',
-    'views/EML211View'], 
-    function($, _, Backbone, DataPackage, DataONEObject, ScienceMetadata, EML211, EML211View) {
+    'models/metadata/eml211/EML211', 'views/DataItemView',
+    'text!templates/dataPackage.html'], 
+    function($, _, Backbone, DataPackage, DataONEObject, ScienceMetadata, EML211, DataItemView, DataPackageTemplate) {
         'use strict';
         
         /*
-            The main view of a Data Package in the editor.  The view is
-            composed of a citation section at the top, a file/folder browser
-            below it, followed by a metadata viewer below that, which has a nav
-            sidebar, and a metadata content section.
-        */
+         *  The main view of a Data Package in the editor.  The view is
+         *  composed of a citation section at the top, a file/folder browser
+         *  below it, followed by a metadata viewer below that, which has a nav
+         *  sidebar, and a metadata content section.
+         */
         var DataPackageView = Backbone.View.extend({
             
-            el: "#data-package-container",
+            tagName: "table",
+            
+            className: "table table-striped table-hover",
+            
+            id: "data-package-table",
             
             dataPackage: null,
             
-            initialize: function(options) {
+            subviews: [],
+            
+            template: _.template(DataPackageTemplate),
+            
+            initialize: function(models, options) {
+                
+                this.listenTo(MetacatUI.rootDataPackage, 'add', this.addOne); // render new items
+                this.listenTo(MetacatUI.rootDataPackage, 'reset', this.AddAll) // render all items
+                                
+                return this;
                 
             },
             
+            /*
+             *  Render the DataPackage HTML
+             */
+            render: function() {
+
+                this.$el.append(this.template());
+                
+                return this;
+            },
+            
+            /*
+             * Add a single DataItemView row to the DataPackageView
+             */
+            addOne: function(item) {
+                var dataItemView = new DataItemView({model: item});
+                this.subviews.push(dataItemView); // keep track of all views
+                $('#data-package-table-body').append(dataItemView.render().el);
+                
+            },
+            
+            /*
+             * Add all rows to the DataPackageView
+             */
+            addAll: function() {
+                this.$('data-package-table-body').html(''); // clear the table first
+                MetacatUI.rootDataPackage.each(this.addOne, this);
+            },
+            
+            close: function() {
+                // Close each subview
+                _.each(this.subviews, function(i, subview) {
+    				subview.close();
+                
+                });
+            
+                this.subviews = [];
+                
+            }
             
         });
         return DataPackageView;
