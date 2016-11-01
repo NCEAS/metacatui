4e04b67dfb2c364e60b03faf77193a092810015e
Christopher Jones <csj@christopherjones.co>
2016-11-01 15:38:44 -0600

    Get the timing right for creating the DataPackageView (now in getDataPackage(), called by render()). Change renderMember() to evaluate the 'type' attribute of the fetched model, and for metadata, call renderMetadata(). Change renderDataPackage() to renderDataPackageItem(), although we may not need this at all (relying on collection.add events).

--- metacatui/src/main/webapp/js/views/EditorView.js
+++ metacatui/src/main/webapp/js/views/EditorView.js
@@ -33,6 +33,9 @@ define(['underscore',
         /* A list of the subviews of the editor */
         subviews: [],
         
+        /* The data package view */
+        dataPackageView: null,
+        
         /* Initialize a new EditorView - called post constructor */
         initialize: function(options) {
             
@@ -48,7 +51,7 @@ define(['underscore',
                 this.createModel();
 
             }
-                        
+
             return this;
         },
         
@@ -80,7 +83,7 @@ define(['underscore',
 	            	this.model.fetch();
 	            });
         	}
-            
+
             //When the basic Solr metadata are retrieved, get the associated package
             this.listenToOnce(this.model, "sync", this.getDataPackage);
                         
@@ -102,7 +105,14 @@ define(['underscore',
                 // Set the root data package for the collection
                 MetacatUI.rootDataPackage = new DataPackage(null, {id: resourceMapIds[0]});
                 // As the root collection is updated with models, render the UI
-                this.listenTo(MetacatUI.rootDataPackage, "update", this.renderMember);
+                this.listenTo(MetacatUI.rootDataPackage, "change", this.renderMember);
+
+                // Render the package table framework
+                this.dataPackageView = new DataPackageView({edit: true});
+                var $packageTableContainer = this.$("#data-package-container");
+                $packageTableContainer.append(this.dataPackageView.render().el);
+                this.subviews.push(this.dataPackageView);
+            
 
                 MetacatUI.rootDataPackage.fetch();
                                 
@@ -114,66 +124,78 @@ define(['underscore',
         /* Calls the appropriate render method depending on the model type */
         renderMember: function(model, collection, options) {
             
-            // Render metadata or package information, based on the packageModel property       
-            if ( typeof model.packageModel === "undefined" ) {
-                this.renderMetadata(model, collection, options);
+            // Render metadata or package information, based on the type
+            
+            if ( typeof model.attributes === "undefined") {
+                return;
                 
             } else {
-                this.renderDataPackage(model, collection, options);
-                
-            }
-            
+                switch ( model.get("type")) {
+                    case "DataPackage":
+                        // Do recursive rendering here for sub packages
+                        break;
+                        
+                    case "Metadata":
+                        
+                        // this.renderDataPackageItem(model, collection, options);
+                        this.renderMetadata(model, collection, options);
+                        break;
+                        
+                    case "Data":
+                        //this.renderDataPackageItem(model, collection, options);
+                        break;
+                    
+                    default:
+                        console.log("model.type is not set correctly");
+                        
+                }
+            }            
         },
         
         /* Renders the metadata section of the EditorView */
         renderMetadata: function(model, collection, options){
             
+            var emlView, dataPackageView;
+
             // render metadata as the collection is updated, but only EML passed from the event
             if ( typeof model.get === "undefined" || 
                         model.get("formatid") !== "eml://ecoinformatics.org/eml-2.1.1" ) {
-                this.listenToOnce(model, "change", this.renderMember);
+                console.log("Not EML. TODO: Render generic ScienceMetadata.");
                 return;
                 
             } else {
             	console.log("Rendering EML Model ", model);
         	
             	//Create an EML211 View and render it
-            	var emlView = new EMLView({ 
+            	emlView = new EMLView({ 
             		model: model,
             		edit: true
             		});
             	this.subviews.push(emlView);
             	emlView.render();
+                // this.renderDataPackageItem(model, collection, options);
                 this.off("change", this.renderMember, model); // avoid double renderings      	
                 
             }
         },
         
         /* Renders the data package section of the EditorView */
-        renderDataPackage: function(model, collection, options) {
+        renderDataPackageItem: function(model, collection, options) {
+                        
+            var hasPackageSubView = 
+                _.find(this.subviews, function(subview) {
+                    return subview.id === "data-package-table";
+                }, model);
             
-            // render data packages passed in from the update event
-            if ( typeof model.packageModel === "undefined" ) {
-                return;
-                
-            }
-            
-            if ( typeof model.packageModel.get === "undefined" ||
-                        model.packageModel.get("formatid") === "undefined" || 
-                        model.packageModel.get("formatid") !== "http://www.openarchives.org/ore/terms" ) {
-                this.listenToOnce(model, "change", this.renderMember);
-                return;
-                
-            } else {
-            	console.log("Rendering Data Package Model ", model);
-                var dataPackageView = new DataPackageView({
-                    collection: dataPackage,
+            // Only create the package table if it hasn't been created            
+            if ( ! hasPackageSubView ) {
+                this.dataPackageView = new DataPackageView({
+                    collection: MetacatUI.rootDataPackage,
                     edit: true});
-                this.subviews.push(dataPackageView);
+                this.subviews.push(this.dataPackageView);
                 dataPackageView.render();
-                this.off("change", this.renderMember, model); // avoid double renderings      	
+                
             }
-            
         },
         
 	    showControls: function(){
