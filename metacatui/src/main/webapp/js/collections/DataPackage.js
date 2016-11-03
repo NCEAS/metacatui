4f6bd1012b45ad221225762590e5f886cc6266cb
Christopher Jones <csj@christopherjones.co>
2016-11-03 17:08:42 -0600

    Use the new JSON structure with the `content` attribute.

--- metacatui/src/main/webapp/js/collections/DataPackage.js
+++ metacatui/src/main/webapp/js/collections/DataPackage.js
@@ -323,7 +323,7 @@ define(['jquery', 'underscore', 'backbone', 'rdflib',
                 console.log("DataPackage.getMember() called for " + context.id);
                 var memberModel = {};
                 
-                switch ( context.get("formatid") ) {
+                switch ( context.get("formatid").content ) {
                     
                     case "http://www.openarchives.org/ore/terms":
                         context.set("type", "DataPackage");
