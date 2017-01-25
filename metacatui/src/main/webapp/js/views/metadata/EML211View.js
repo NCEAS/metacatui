b030b8d2dd8d5c5c69dc250c4d1bb5761cf4694c
Bryce Mecum <petridish@gmail.com>
2017-01-25 07:19:06 -0900

    Tidy up formatting of the EML211View

--- metacatui/src/main/webapp/js/views/metadata/EML211View.js
+++ metacatui/src/main/webapp/js/views/metadata/EML211View.js
@@ -4,22 +4,118 @@ define(['underscore', 'jquery', 'backbone',
         'models/metadata/eml211/EMLText',
         'models/metadata/eml211/EMLTemporalCoverage',
         'text!templates/eml.html',
-    
-    var EMLView = ScienceMetadataView.extend({
-    	
-    	type: "EML211",
-        
-        el: '#metadata-container',
-        
-        /* Templates */
-        
         'text!templates/metadataOverview.html'
     ],
     function (_, $, Backbone, ScienceMetadataView, EML, EMLText, EMLTemporalCoverage, Template, OverviewTemplate) {
+        var EMLView = ScienceMetadataView.extend({
+
+            type: "EML211",
+
+            el: '#metadata-container',
+
             events: {
                 "change .text": "updateText",
                 "change .temporal": "updateTemporalCoverage"
             },
+
+            /* A list of the subviews */
+            subviews: [],
+
+            /* Templates */
+            template: _.template(Template),
+            overviewTemplate: _.template(OverviewTemplate),
+
+            initialize: function (options) {
+
+                //Set up all the options
+                if (typeof options == "undefined") var options = {};
+
+                //The EML Model and ID
+                this.model = options.model || new EML();
+                if (!this.model.get("id") && options.id) this.model.set("id", options.id);
+
+                //This is a new EML doc being created
+                if (!this.model.get("id")) console.log("No data set id is specified");
+
+                //Get the current mode
+                this.edit = options.edit || false;
+
+                return this;
+            },
+
+            /* Render the view */
+            render: function () {
+                console.log(this.model);
+                MetacatUI.appModel.set('headerType', 'default');
+
+                //Render the basic structure of the page and table of contents
+                this.$el.html(this.template());
+                this.$container = this.$(".metadata-container");
+
+                //Render all the EML sections when the model is synced
+                this.renderAllSections();
+                this.listenTo(this.model, "sync", this.renderAllSections);
+
+                return this;
+            },
+
+            renderAllSections: function () {
+                this.renderOverview();
+                this.renderPeople();
+                this.renderDates();
+                this.renderLocations();
+                this.renderTaxa();
+                this.renderMethods();
+                this.renderProject();
+                this.renderSharing();
+            },
+
+            /*
+             * Renders the Overview section of the page
+             */
+            renderOverview: function () {
+                //Get the overall view mode
+                var edit = this.edit;
+
+                //Append the empty layout
+                var overviewEl = this.$container.find(".overview");
+                $(overviewEl).html(this.overviewTemplate());
+
+                //Abstract
+                _.each(this.model.get("abstract"), function (abs) {
+                    var abstractEl = this.createEMLText(abs, edit, "abstract");
+
+                    //Add the abstract element to the view
+                    $(overviewEl).find(".abstract").append(abstractEl);
+                }, this);
+
+                if (!this.model.get("abstract").length) {
+                    var abstractEl = this.createEMLText(null, edit, "abstract");
+
+                    //Add the abstract element to the view
+                    $(overviewEl).find(".abstract").append(abstractEl);
+                }
+
+                //Keywords
+                var keywords = this.createKeywords(edit);
+                $(overviewEl).find(".keywords").append(keywords);
+
+                //Alternate Ids
+                var altIds = this.createAltIds(edit);
+                $(overviewEl).find(".altids").append(altIds);
+
+                //Usage
+                var usage = this.createUsage(edit);
+                $(overviewEl).find(".usage").append(usage);
+            },
+
+            /*
+             * Renders the People section of the page
+             */
+            renderPeople: function () {
+
+            },
+
             /*
              * Renders the Dates section of the page
              */
@@ -32,6 +128,42 @@ define(['underscore', 'jquery', 'backbone',
                 var datesEl = this.$container.find(".dates");
                 $(datesEl).html(this.createDates());
             },
+
+            /*
+             * Renders the Locations section of the page
+             */
+            renderLocations: function () {
+
+            },
+
+            /*
+             * Renders the Taxa section of the page
+             */
+            renderTaxa: function () {
+
+            },
+
+            /*
+             * Renders the Methods section of the page
+             */
+            renderMethods: function () {
+
+            },
+
+            /*
+             * Renders the Projcet section of the page
+             */
+            renderProject: function () {
+
+            },
+
+            /*
+             * Renders the Sharing section of the page
+             */
+            renderSharing: function () {
+
+            },
+
             // Create the HTML for the dataset temporal coverage information
             createDates: function () {
                 var edit = this.edit;
@@ -69,6 +201,95 @@ define(['underscore', 'jquery', 'backbone',
 
                 return ele;
             },
+
+            createKeywords: function () {
+                //Keywords
+                var keywords = this.model.get("keywordset"),
+                    keywordInputTemp = $(document.createElement("input")).attr("type", "text").addClass("keyword span10"),
+                    thesInputTemp = $(document.createElement("select")).addClass("thesaurus span2").append(
+                        $(document.createElement("option")).val("none").text("None")).append(
+                        $(document.createElement("option")).val("GCMD").text("GCMD")),
+                    keywordsForm = $(document.createElement("div")).addClass("row-fluid");
+
+                //Iterate over each keyword and add a text input for the keyword value and a dropdown menu for the thesaurus
+                _.each(keywords, function (keyword) {
+                    var keywordInput = $(keywordInputTemp).clone().val(keyword.keyword);
+                    var thesInput = $(thesInputTemp).clone();
+
+                    //Get the thesaurus value
+                    if (keyword.keywordthesaurus && keyword.keywordthesaurus.indexOf("GCMD") > -1)
+                        thesInput.val("GCMD");
+
+                    keywordsForm.append(keywordInput, thesInput);
+                });
+
+                keywordsForm.append($(keywordInputTemp).clone().attr("placeholder", "Add a new keyword"), $(thesInputTemp).clone());
+
+                return keywordsForm;
+            },
+
+            createAltIds: function () {
+                return "";
+            },
+
+            createUsage: function () {
+                return "";
+            },
+
+
+            /*
+             * Creates the text elements
+             */
+            createEMLText: function (textModel, edit, category) {
+
+                if (!textModel && edit) {
+                    return $(document.createElement("textarea"))
+                        .attr("data-category", category)
+                        .addClass("xlarge text");
+                } else if (!textModel && !edit) {
+                    return $(document.createElement("div"))
+                        .attr("data-category", category);
+                }
+
+                //Get the EMLText from the EML model
+                var finishedEl;
+
+                //Get the text attribute from the EMLText model
+                var paragraphs = textModel.get("text");
+
+                //If there is no text, then just return an empty div
+                if (!paragraphs.length) return $(document.createElement("div"));
+
+                //If the text should be editable,
+                if (edit) {
+                    //Format the paragraphs with carriage returns between paragraphs
+                    var paragraphsString = _.reduce(paragraphs, function (p) {
+                        return p + String.fromCharCode(13);
+                    });
+
+                    //Create the textarea element
+                    finishedEl = $(document.createElement("textarea"))
+                        .addClass("xlarge text")
+                        .attr("data-category", category)
+                        .html(paragraphsString);
+                } else {
+                    //Format the paragraphs with HTML
+                    var paragraphsString = _.reduce(paragraphs, function (p) {
+                        return "<p>" + p + "</p>";
+                    });
+                    finishedEl = $(document.createElement("div"))
+                        .attr("data-category", category)
+                        .append(paragraphsString);
+                }
+
+                $(finishedEl).data({
+                    model: textModel
+                });
+
+                //Return the finished DOM element
+                return finishedEl;
+            },
+
             // Create the date(time) elements
             // Currently only supports dates, not datetimes. This is because HTML5 
             // separates out the date input type from time and there is no longer a 
@@ -126,7 +347,106 @@ define(['underscore', 'jquery', 'backbone',
                 return finishedEl;
             },
 
+            /*
+             * Updates a basic text field in the EML after the user changes the value
+             */
+            updateText: function (e) {
+                console.log('before', this.model.get('abstract'));
+
+                if (!e) return false;
+
+                var category = $(e.target).attr("data-category"),
+                    textModel = $(e.target).data("model"),
+                    value = $(e.target).val().trim();
+
+                //We can't update anything without a category
+                if (!category) return false;
+
+                //Get the list of paragraphs - checking for carriage returns and line feeds
+                var paragraphsCR = value.split(String.fromCharCode(13));
+                var paragraphsLF = value.split(String.fromCharCode(10));
+
+                //Use the paragraph list that has the most
+                var paragraphs = (paragraphsCR > paragraphsLF) ? paragraphsCR : paragraphsLF;
+
+                //If this category isn't set yet, then create a new EMLText model
+                if (!textModel) {
+                    console.log('not textmodel', category)
+                    this.model.set(category,
+                        new EMLText({
+                            text: paragraphs,
+                            parentModel: this.model,
+                            parentAttribute: category
+                        }));
+
                     // Save the new model onto the underlying DOM node
+                    $(e.target).data({ "model" : this.model.get(category) });
+                //Update the existing EMLText model
+                } else {
+                    textModel.set("text", paragraphs);
+                    textModel.trigger("change:text");
+                }
+
+                console.log('after', this.model.get('abstract'));
+
+            },
+
+            // Update an EMLTemporalCoverage instance
+            updateTemporalCoverage: function (e) {
+                console.log('updateTemporalCoverage');
+
+                if (!e) return false;
+
+                // Grab the values we need to update the underlying EMLTemporalCoverage
+                // model. Notice that we use the 'data-category' attribute on the parent
+                // div to figure out if this is the begin or end date and that use the
+                // 'type' attribute on the input element to determine whether this is a
+                // calendarDate or a time that's being updated
+                var parent = $(e.target).parent(),
+                    category = parent.attr("data-category"),
+                    model = parent.data("model"),
+                    attrib = $(e.target).attr('type'),
+                    value = $(e.target).val().trim();
+
+                // We can't update anything without a category
+                if (!category) return false;
+
+                // Map natural values of 'attrib' to the corresponding attribute on the
+                // EMLTemporalCoverage element. Natural values of 'attrib' are from the
+                // HTML5 input 'type' attribute: Either 'date' or 'time'. Corresponding
+                // attributes on the EMLTemporalCoverage model are from the EML schema
+                // and are either 'calendarDate' or 'time'.
+                if (attrib == 'date') attrib = 'calendarDate';
+
+                // If this datetime isn't paired with an existing
+                // EMLTemporalCoverage instance, create one. Otherwise, mutate
+                // the existing one
+                if (!model) {
+                    // Create a new Object for the the attribute on the
+                    // EMLTemporalCoverage instance in 'attrib' (either
+                    // beginDate or endDate). Then merge it into the model and
+                    // merge the EMLTemporalCoverage model into the EML model.
+                    newval = {}; 
+                    newval[attrib] = value;
+                    model.set(category, newval);
+
+                    model = new EMLTemporalCoverage({});
+                    this.model.set('temporalCoverage', 
+                                   this.model.get('temporalCoverage').concat(model));
+
+                    // Save the new model onto the underlying DOM node
+                    parent.data({ 'model': model });
+                } else {
+                    // Grab the current value from the model,
+                    // x = { calendarDate : ..., time: ... }
+                    // and merge in the new value
+                    var currentValue = model.get(category);
+                    currentValue[attrib] = value;
+
+                    // Set the merged value
+                    model.set(category, currentValue);
+                }
+                
                 
         /* A list of the subviews */
         subviews: [],
