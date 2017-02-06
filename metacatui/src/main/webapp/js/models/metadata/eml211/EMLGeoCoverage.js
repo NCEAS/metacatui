28d367c310e92ae22591953c125af8996598cbd8
leinfelder <leinfelder@nceas.ucsb.edu>
2017-02-06 14:15:03 -0800

    add implementations to updateDOM() and parse() methods

--- metacatui/src/main/webapp/js/models/metadata/eml211/EMLGeoCoverage.js
+++ metacatui/src/main/webapp/js/models/metadata/eml211/EMLGeoCoverage.js
@@ -7,7 +7,12 @@ define(['jquery', 'underscore', 'backbone', 'models/DataONEObject'],
 		defaults: {
 			objectXML: null,
 			objectDOM: null,
-			parentModel: null
+			parentModel: null,
+        	geographicDescription: null,
+    		eastBoundingCoordinate: null,
+        	northBoundingCoordinate: null,
+        	southBoundingCoordinate: null,
+        	westBoundingCoordinate: null
 		},
 		
 		initialize: function(attributes){
@@ -33,9 +38,35 @@ define(['jquery', 'underscore', 'backbone', 'models/DataONEObject'],
             }
         },
 		
-		parse: function(objectDOM){
-			if(!objectDOM) var objectDOM = this.get("objectDOM");
+        /** Based on this example serialization
+		<geographicCoverage scope="document">
+			<geographicDescription>Rhine-Main-Observatory</geographicDescription>
+			<boundingCoordinates>
+				<westBoundingCoordinate>9.0005</westBoundingCoordinate>
+				<eastBoundingCoordinate>9.0005</eastBoundingCoordinate>
+				<northBoundingCoordinate>50.1600</northBoundingCoordinate>
+				<southBoundingCoordinate>50.1600</southBoundingCoordinate>
+			</boundingCoordinates>
+		</geographicCoverage>
+		 **/
+		parse: function(objectDOM) {
 			
+			var modelJSON = {};
+
+			if (!objectDOM) var objectDOM = this.get("objectDOM");
+			
+			var geographicDescription = $(objectDOM).children('geographicDescription');
+			modelJSON.geographicDescription = geographicDescription;
+			
+			var boundingCoordinates = $(objectDOM).children('boundingCoordinates');
+			if (boundingCoordinates) {
+				modelJSON.eastBoundingCoordinate = $(objectDOM).children('eastBoundingCoordinate').text();
+				modelJSON.northBoundingCoordinate = $(objectDOM).children('northBoundingCoordinate').text();
+				modelJSON.southBoundingCoordinate = $(objectDOM).children('southBoundingCoordinate').text();
+				modelJSON.westBoundingCoordinate = $(objectDOM).children('westBoundingCoordinate').text();
+			}
+			
+			return modelJSON;
 		},
 		
 		serialize: function(){
@@ -52,7 +83,31 @@ define(['jquery', 'underscore', 'backbone', 'models/DataONEObject'],
 		 * Makes a copy of the original XML DOM and updates it with the new values from the model.
 		 */
 		updateDOM: function(){
-			 var objectDOM = this.get("objectDOM").cloneNode(true);
+			var objectDOM;
+
+			if (this.get("objectDOM")) {
+				objectDOM = this.get("objectDOM").cloneNode(true);
+			} else {
+				objectDOM = $("<geographicCoverage></geographicCoverage>");
+			}
+			
+			// description
+			$(objectDOM).find("geographicDescription").text(this.get("geographicDescription"));
+			
+			// bounds
+			if (!$(objectDOM).find("boundingCoordinates")) {
+				$(objectDOM).append("<boundingCoordinates/>");
+			}
+			var boundingCoordinates = $(objectDOM).find("boundingCoordinates");
+			$(boundingCoordinates).append("<westBoundingCoordinate/>");
+			$(boundingCoordinates).append("<eastBoundingCoordinate/>");
+			$(boundingCoordinates).append("<northBoundingCoordinate/>");
+			$(boundingCoordinates).append("<southBoundingCoordinate/>");
+
+			$(objectDOM).find("westBoundingCoordinate").text(this.get("westBoundingCoordinate"));
+			$(objectDOM).find("eastBoundingCoordinate").text(this.get("eastBoundingCoordinate"));
+			$(objectDOM).find("northBoundingCoordinate").text(this.get("northBoundingCoordinate"));
+			$(objectDOM).find("southBoundingCoordinate").text(this.get("southBoundingCoordinate"));
 			 
 			 return objectDOM;
 		},
