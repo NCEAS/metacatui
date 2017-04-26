aceb55207c0c1d079ca1a13494ac8e6ba0b63e72
Christopher Jones <csj@christopherjones.co>
2017-04-26 14:27:02 -0600

    Write the EMLEntity.spec tests
    To get started with the testing framework, create a spec file for the
    EMLEntity object.  So far, est the constructor and the parse() method.
    To seed the response for parse(), I created the EntityUtil object and
    added a getTestEntityXML() method.  We’ll need to think about how we
    want to do this - perhaps use a shared JS file across tests - we’ll see.

---
+++ metacatui/test/js/specs/models/metadata/eml211/EMLEntity.spec.js
@@ -0,0 +1,84 @@
+"use strict";
+
+define(["chai", "chai-jquery", "chai-backbone",
+    "../../../../../../src/main/webapp/js/models/metadata/eml211/EMLEntity"],
+    function(chai, chaiJquery, chaiBackbone, EMLEntity) {
+
+        // Configure the Chai assertion library
+        var should =  chai.should();
+        var expect = chai.expect;
+
+        // Pull in Jquery and Backbone-specific assertion libraries
+        chai.use(chaiJquery); // exported from chai-jquery.js
+        chai.use(chaiBackbone); // exported from chai-backbone.js
+
+        describe("EMLEntity Test Suite", function (){
+            var emlEntity = new EMLEntity();
+            var responseXML; // mock response from the server
+            var attributes; // object returned by EMLEntity.parse()
+
+            /* Setup */
+            before(function() {
+                // If needed
+                responseXML = EntityUtil.getTestEntityXML();
+                attributes = emlEntity.parse(responseXML);
+
+            });
+
+            /* Tear down */
+            after(function() {
+                // If needed
+            });
+
+            describe("The EMLEntity object", function() {
+                it('should exist', function() {
+                    expect(emlEntity).to.exist;
+                    emlEntity.should.exist;
+                });
+            });
+
+            describe(".parse()", function() {
+                it("should return an attribute object", function() {
+                    attributes.should.be.an("object");
+
+                });
+
+                it("should return an alternate identifier array", function() {
+                    attributes.alternateidentifier.should.be.an("array");
+                    attributes.alternateidentifier[0].should.equal("altid.1.1.png");
+                    attributes.alternateidentifier[1].should.equal("altid2.1.1.png");
+                });
+
+                it("should return an entity name", function() {
+                    attributes.entityname.should.be.a("string");
+                    attributes.entityname.should.equal("entity.1.1.png");
+                });
+
+                it("should return an entity description", function() {
+                    attributes.entitydescription.should.be.a("string");
+                    attributes.entitydescription.should.equal("A descrition of entity.1.1.png");
+                });
+
+                it("should return an entity type", function() {
+                    attributes.entitytype.should.be.a("string");
+                    attributes.entitytype.should.equal("Portable Network graphic image");
+                });
+            });
+        });
+
+        var EntityUtil = {
+            getTestEntityXML: function() {
+                var xml = [];
+                xml.push(
+                    "<otherEntity>\n",
+                    "\t<alternateIdentifier>altid.1.1.png</alternateIdentifier>\n",
+                    "\t<alternateIdentifier>altid2.1.1.png</alternateIdentifier>\n",
+                    "\t<entityName>entity.1.1.png</entityName>\n",
+                    "\t<entityDescription>A descrition of entity.1.1.png</entityDescription>\n",
+                    "\t<entityType>Portable Network graphic image</entityType>\n",
+                    "</otherEntity>");
+
+                return xml.join('');
+            }
+        }
+    });
