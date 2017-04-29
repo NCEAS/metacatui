ae4417e5fe83b59ecefbff77fe60850c32ecbf78
Christopher Jones <csj@christopherjones.co>
2017-04-29 15:09:14 -0600

    Create an EMLMeasurementScale factory
    EMLMeasurementScale provides a static factory method for producing an
    appropriate measurement scale object based on the incoming
    <measurementScale> XML fragment.  For nominal and ordinal scales, it
    returns an EMLNonNumericDomain object.  For interval and ratio scales
    it returns an EMLNumericDomain object.  For a dateTime scale, it
    returns an EMLDateTimeDomain object. This factory is used in
    EMLAttribute.parse() to create the correct measurementScale attribute.
    
    refs https://github.com/NCEAS/metacatui/issues/87

---
+++ metacatui/src/main/webapp/js/models/metadata/eml211/EMLMeasurementScale.js
@@ -0,0 +1,71 @@
+define(["jquery", "underscore", "backbone",
+    "models/metadata/eml211/EMLNonNumericDomain",
+    "models/metadata/eml211/EMLNumericDomain",
+        "models/metadata/eml211/EMLDateTimeDomain"],
+    function($, _, Backbone, EMLNonNumericDomain, EMLNumericDomain, EMLDateTimeDomain) {
+
+        /*
+         * EMLMeasurementScale is a measurement scale factory that returns
+         * an EMLMeasurementScale subclass of either EMLNonNumericDomain,
+         * EMLNumericDomain, or EMLDateTimeDomain, depending on the
+         * domain name found in the given measurementScaleXML
+         */
+        var EMLMeasurementScale = Backbone.Model.extend({},
+
+        {
+            /*
+             * Get an instance of an EMLMeasurementScale subclass
+             * given the measurementScaleXML fragment
+             */
+            getInstance: function(measurementScaleXML) {
+                var instance = {};
+                var options = {parse: true};
+                var $measurementScale = $(measurementScaleXML);
+                var domainName = $measurementScale.children()[0].localName;
+
+                // Return the appropriate sub class of EMLMeasurementScale
+                switch ( domainName ) {
+                    case "nominal":
+                        instance = new EMLNonNumericDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": measurementScaleXML
+                        }, options);
+                        break;
+                    case "ordinal":
+                        instance = new EMLNonNumericDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": measurementScaleXML
+                        }, options);
+                        break;
+                    case "interval":
+                        instance = new EMLNumericDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": measurementScaleXML
+                        }, options);
+                        break;
+                    case "ratio":
+                        instance = new EMLNumericDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": measurementScaleXML
+                        }, options);
+                        break;
+                    case "dateTime":
+                        instance = new EMLDateTimeDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": measurementScaleXML
+                        }, options);
+                        break;
+                    default:
+                        instance = new EMLNonNumericDomain({
+                            "measurementScale": domainName,
+                            "objectDOM": $measurementScale
+                        }, options);
+                }
+
+                return instance;
+            }
+        });
+
+        return EMLMeasurementScale;
+    }
+);
