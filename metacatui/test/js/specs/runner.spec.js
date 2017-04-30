69db7f02e2e4a3c54c151da6ad82932df89064f6
Christopher Jones <csj@christopherjones.co>
2017-04-30 08:41:36 -0600

    Update the spec runner
    Add in the EMLNonNumericDomain unit test.

--- metacatui/test/js/specs/runner.spec.js
+++ metacatui/test/js/specs/runner.spec.js
@@ -46,6 +46,7 @@ define(["require", "mocha"], function(require, mocha) {
             "js/specs/models/metadata/eml211/EMLEntity.spec",
             "js/specs/models/metadata/eml211/EMLAttribute.spec",
             "js/specs/models/metadata/eml211/EMLMeasurementScale.spec",
+            "js/specs/models/metadata/eml211/EMLNonNumericDomain.spec",
             "js/specs/models/metadata/eml211/EMLDateTimeDomain.spec"
         ];
 
