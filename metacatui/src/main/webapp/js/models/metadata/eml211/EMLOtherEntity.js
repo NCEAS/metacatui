c2a3d087f396c457ca17993533d9aa45be1764fc
Christopher Jones <csj@christopherjones.co>
2017-05-02 07:02:59 -0600

    Add an EMLOtherEntity object
    This object subclasses EMLEntity, and extends its defaults and
    nameNodeMap attributes. It corresponds to the EML otherEntity module,
    and adds the entityType attribute.

---
+++ metacatui/src/main/webapp/js/models/metadata/eml211/EMLOtherEntity.js
@@ -0,0 +1,81 @@
+define(["jquery", "underscore", "backbone", "models/metadata/eml211/EMLEntity"],
+    function($, _, Backbone, EMLEntity) {
+
+        /*
+         * EMLOtherEntity represents a generic data entity, corresponding
+         * with the EML otherEntity module.
+         *
+         * @see https://github.com/NCEAS/eml/blob/master/eml-entity.xsd
+         */
+        var EMLOtherEntity = EMLEntity.extend({
+
+            /* Attributes of any entity */
+            defaults: _.extend({
+
+                /* Attributes from EML */
+                entityType: null // The type of the entity
+
+                /* Attributes not from EML */
+
+            }, EMLEntity.prototype.defaults),
+
+            /*
+             * The map of lower case to camel case node names
+             * needed to deal with parsing issues with $.parseHTML().
+             * Use this until we can figure out issues with $.parseXML().
+             */
+            nodeNameMap: _.extend({
+                "entitytype": "entityType"
+
+            }, EMLEntity.prototype.nodeNameMap),
+
+            /* Initialize an EMLOtherEntity object */
+            initialize: function(attributes) {
+
+                // if options.parse = true, Backbone will call parse()
+
+                // Call super() first
+                this.constructor.__super__.initialize.apply(this, attributes);
+
+                // EMLOtherEntity-specific work
+
+                // Register change events
+                this.on( "change:entityType", EMLEntity.trickleUpChange);
+            },
+
+            /*
+             * Parse the incoming other entity's XML elements
+             */
+            parse: function(objectDOM) {
+
+
+                // Call super() first
+                this.constructor.__super__.parse.apply(this, objectDOM);
+
+                // EMLOtherEntity-specific work
+                var modelJSON  = {}; // the attributes to return
+                var $objectDOM;
+
+                // Use the cached object if we have it
+                if ( !objectDOM ) {
+                    if ( this.get("objectDOM") ) {
+                        objectDOM = this.get("objectDOM");
+
+                    } else {
+                        return {};
+                    }
+                }
+
+                $objectDOM = $(objectDOM);
+
+                // Add the entityType
+                modelJSON.entityType = $objectDOM.children("entitytype").text();
+
+                return modelJSON;
+            }
+
+        });
+
+        return EMLOtherEntity;
+    }
+);
