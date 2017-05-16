b1f3e415afc4cb2a1e481513e477c026c1af98de
Christopher Jones <csj@christopherjones.co>
2017-05-16 11:09:16 -0600

    Fix EMLMeasurementScale unit tests
    EMLMeasurementScale no longer needs to use a DOMParser since we have a
    temporary hack for <source> elements. When creating new instances, pass
    in the objectDOM, not objectXML.
    
    Note: @laurenwalker I see you changed the factory to produce objects by
    passing in the domainName only, with no XML fragment. I’m thinking that
    the factory should be used for existing content where we pass in the
    fragment, and that we directly instantiate EMLNonNumericDomain,
    EMLNumericDomain, and EMLDateTime using their respective constructors
    directly if we a starting with fresh new objects.  Let’s discuss.

--- metacatui/src/main/webapp/js/models/metadata/eml211/EMLMeasurementScale.js
+++ metacatui/src/main/webapp/js/models/metadata/eml211/EMLMeasurementScale.js
@@ -19,25 +19,10 @@ define(["jquery", "underscore", "backbone",
              */
             getInstance: function(measurementScaleXML) {
                 var instance = {};
-                
-                /* JQuery seems to have a bug handling XML elements named "source"
-                 * $objectDOM = $(attributes.objectDOM) gives us:
-                 * <nominal>
-                 * ....<nonnumericdomain>
-                 * ........<textdomain>
-                 * ............<definition>Any text</definition>
-                 * ............<pattern>*</pattern>
-                 * ............<source>Any source
-                 * ........</textdomain>
-                 * ....</nonnumericdomain>
-                 * </nominal>
-                 * Note the lost </source>. Changing the element name to "sourced" works fine.
-                 * Use the DOMParser instead
-                 */
+
                 if(measurementScaleXML && measurementScaleXML.indexOf("<") > -1){
-                    var parser = new DOMParser();
-                    var parsedDOM = parser.parseFromString(measurementScaleXML, "text/xml");
-                	var domainName = $(parsedDOM).find("measurementscale").children()[0].localName;
+                    var objectDOM = $(measurementScaleXML)[0];
+                	var domainName = $(objectDOM).children()[0].localName;
                 	var options = {parse: true};
                 }
                 //If it's not an XML string, then it must be the domainName itself
@@ -46,43 +31,43 @@ define(["jquery", "underscore", "backbone",
                 	var options = {};
                 	measurementScaleXML = null;
                 }
-                
+
                 // Return the appropriate sub class of EMLMeasurementScale
                 switch ( domainName ) {
                     case "nominal":
                         instance = new EMLNonNumericDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                         break;
                     case "ordinal":
                         instance = new EMLNonNumericDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                         break;
                     case "interval":
                         instance = new EMLNumericDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                         break;
                     case "ratio":
                         instance = new EMLNumericDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                         break;
                     case "datetime":
                         instance = new EMLDateTimeDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                         break;
                     default:
                         instance = new EMLNonNumericDomain({
                             "measurementScale": domainName,
-                            "objectXML": measurementScaleXML
+                            "objectDOM": $(measurementScaleXML)[0]
                         }, options);
                 }
 
