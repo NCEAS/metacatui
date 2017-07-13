007d0d4431e31c105d018fcf939c856be75e226e
Christopher Jones <csj@christopherjones.co>
2017-07-13 10:48:02 -0600

    Create a unit object
    We need to be able to discern between standard and custom units, and so
    EMLNumericDomain.parse() creates a plain unit object like:
    {standardUnit: “celcius”}
    or a custom unit like:
    {customUnit: “applesPerSquareMeter”}
    Change EMLMeasurementScaleView to produce the same unit object
    structure, and we’ll later handle custom units.

--- metacatui/src/main/webapp/js/views/metadata/EMLMeasurementScaleView.js
+++ metacatui/src/main/webapp/js/views/metadata/EMLMeasurementScaleView.js
@@ -445,7 +445,7 @@ define(['underscore', 'jquery', 'backbone',
             	//Update the standard unit
             	if(updatedInput.is(".units")){
             		var chosenUnit = updatedInput.val();
-            		this.model.set("unit", chosenUnit);
+            		this.model.set("unit", {standardUnit: chosenUnit});
             	}
             	//Update the datetime format
             	else if(updatedInput.is(".datetime")){
