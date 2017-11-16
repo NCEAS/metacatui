2ffc3b5cb2525d08ba9a431731e8b3848c2bb8a3
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2017-11-16 09:09:37 -0800

    Added the property description
    Reference #258

--- metacatui/src/main/webapp/js/models/AppModel.js
+++ metacatui/src/main/webapp/js/models/AppModel.js
@@ -32,6 +32,7 @@ define(['jquery', 'underscore', 'backbone'],
 						
 			maxDownloadSize: 3000000000,
 			
+			// set this variable to true, if the content being published is moderated by the data team.
 			contentIsModerated: false,
 			
 			/*
