82204b5294f88599ac7f6ec2686a1e1a742008fb
Val Hendrix <vchendrix@lbl.gov>
2018-02-15 12:28:18 -0800

    Fixed issue in `EML211.parse()` for additionalinfo parsing
    Closes #493

--- src/js/models/metadata/eml211/EML211.js
+++ src/js/models/metadata/eml211/EML211.js
@@ -331,24 +331,20 @@ define(['jquery', 'underscore', 'backbone', 'uuid',
             		//Parse EMLText modules
             		else if(_.contains(emlText, thisNode.localName)){
             			if(typeof modelJSON[thisNode.localName] == "undefined") modelJSON[thisNode.localName] = [];
-            			
-            			var emlText = new EMLText({ 
-	            				objectDOM: thisNode, 
+
+            			modelJSON[thisNode.localName].push(new EMLText({
+	            				objectDOM: thisNode,
 	            				parentModel: model
-            				});
-            			modelJSON[thisNode.localName].push(emlText);
-            			
-            			
+            				}));
+
             		}
 					else if(_.contains(emlMethods, thisNode.localName)) {
 						if(typeof modelJSON[thisNode.localName] === "undefined") modelJSON[thisNode.localName] = [];
 						
-						var emlMethods = new EMLMethods({
+						modelJSON[thisNode.localName] =  new EMLMethods({
 							objectDOM: thisNode,
 							parentModel: model
-						})
-
-						modelJSON[thisNode.localName] = emlMethods;
+						});
 	
 					}
             		//Parse keywords
