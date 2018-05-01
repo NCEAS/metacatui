048b8821beb4c2b14877532f9d40ea30b178ab60
laurenwalker <walker@nceas.ucsb.edu>
2018-05-01 18:10:36 -0400

    Change version to 1.14.15

--- metacatui/src/main/webapp/loader.js
+++ metacatui/src/main/webapp/loader.js
@@ -9,10 +9,10 @@ var theme 		   = document.getElementById("loader").getAttribute("data-theme");
 var metacatContext = document.getElementById("loader").getAttribute("data-metacat-context");
 var mapKey 		   = document.getElementById("loader").getAttribute("data-map-key");
 if ((mapKey == "YOUR-GOOGLE-MAPS-API-KEY") || (!mapKey)) mapKey = null;
-var useD3 = true; 
+var useD3 = true;
 
 //This version of Metacat UI - used for cache busting
-window.metacatUIVersion = "1.14.14";
+window.metacatUIVersion = "1.14.15";
 
 // Step 2: let everything else be taken care of by the app
 preventCompatibilityIssues();
@@ -31,7 +31,7 @@ function loadTheme(theme) {
 	    else initApp();
     }
 }
-function initApp() {			
+function initApp() {
     var script = document.createElement("script");
     script.setAttribute("data-main", "js/app.js?v=" + window.metacatUIVersion);
     script.src = "components/require.js";
@@ -41,23 +41,23 @@ function initApp() {
 
 // Fix compatibility issues with mainly IE 8 and earlier. Do this before the rest of the app loads since even common
 // functions are missing, such as console.log
-function preventCompatibilityIssues(){	
+function preventCompatibilityIssues(){
 	// Detecting IE
 	function isIE () {
 		  var myNav = navigator.userAgent.toLowerCase();
 		  return (myNav.indexOf('msie') != -1) ? parseInt(myNav.split('msie')[1]) : false;
-	}	
+	}
 	//If IE 8 or earlier, don't use D3
 	if (isIE() && (isIE() < 9)) useD3 = false;
-    
-	
+
+
 	/* Add trim() function for IE*/
 	if(typeof String.prototype.trim !== 'function') {
 		  String.prototype.trim = function() {
-		    return this.replace(/^\s+|\s+$/g, ''); 
+		    return this.replace(/^\s+|\s+$/g, '');
 		  }
 	}
-	
+
 	/* Polyfill for startsWith() - IE 8 and earlier */
 	if (!String.prototype.startsWith) {
 		  String.prototype.startsWith = function(searchString, position) {
@@ -65,7 +65,7 @@ function preventCompatibilityIssues(){
 		    return this.indexOf(searchString, position) === position;
 		  };
 		}
-	
+
 	/* Polyfill for endsWith() - IE 8 and earlier */
 	if (!String.prototype.endsWith) {
 		  String.prototype.endsWith = function(searchString, position) {
@@ -78,14 +78,14 @@ function preventCompatibilityIssues(){
 		      return lastIndex !== -1 && lastIndex === position;
 		  };
 		}
-	
+
 	/* POlyfill for Array.isArray() - IE 8 and earlier */
 	if (!Array.isArray) {
 		  Array.isArray = function(arg) {
 		    return Object.prototype.toString.call(arg) === '[object Array]';
 		  };
 		}
-	
+
 	/**
 	 * Protect window.console method calls, e.g. console is not defined on IE
 	 * unless dev tools are open, and IE doesn't define console.debug
@@ -104,10 +104,10 @@ function preventCompatibilityIssues(){
 	  for (var i = 0; i < m.length; i++) {
 	    if (!window.console[m[i]]) {
 	      window.console[m[i]] = function() {};
-	    }    
-	  } 
+	    }
+	  }
 	})();
-	
+
 	//Add a polyfill for the .map() function for arrays for IE 8. Taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
 	// Production steps of ECMA-262, Edition 5, 15.4.4.19
 	// Reference: http://es5.github.io/#x15.4.4.19
@@ -121,11 +121,11 @@ function preventCompatibilityIssues(){
 	      throw new TypeError(" this is null or not defined");
 	    }
 
-	    // 1. Let O be the result of calling ToObject passing the |this| 
+	    // 1. Let O be the result of calling ToObject passing the |this|
 	    //    value as the argument.
 	    var O = Object(this);
 
-	    // 2. Let lenValue be the result of calling the Get internal 
+	    // 2. Let lenValue be the result of calling the Get internal
 	    //    method of O with the argument "length".
 	    // 3. Let len be ToUint32(lenValue).
 	    var len = O.length >>> 0;
@@ -141,8 +141,8 @@ function preventCompatibilityIssues(){
 	      T = thisArg;
 	    }
 
-	    // 6. Let A be a new array created as if by the expression new Array(len) 
-	    //    where Array is the standard built-in constructor with that name and 
+	    // 6. Let A be a new array created as if by the expression new Array(len)
+	    //    where Array is the standard built-in constructor with that name and
 	    //    len is the value of len.
 	    A = new Array(len);
 
@@ -156,35 +156,35 @@ function preventCompatibilityIssues(){
 
 	      // a. Let Pk be ToString(k).
 	      //   This is implicit for LHS operands of the in operator
-	      // b. Let kPresent be the result of calling the HasProperty internal 
+	      // b. Let kPresent be the result of calling the HasProperty internal
 	      //    method of O with argument Pk.
 	      //   This step can be combined with c
 	      // c. If kPresent is true, then
 	      if (k in O) {
 
-	        // i. Let kValue be the result of calling the Get internal 
+	        // i. Let kValue be the result of calling the Get internal
 	        //    method of O with argument Pk.
 	        kValue = O[k];
 
-	        // ii. Let mappedValue be the result of calling the Call internal 
-	        //     method of callback with T as the this value and argument 
+	        // ii. Let mappedValue be the result of calling the Call internal
+	        //     method of callback with T as the this value and argument
 	        //     list containing kValue, k, and O.
 	        mappedValue = callback.call(T, kValue, k, O);
 
 	        // iii. Call the DefineOwnProperty internal method of A with arguments
-	        // Pk, Property Descriptor 
-	        // { Value: mappedValue, 
-	        //   Writable: true, 
-	        //   Enumerable: true, 
+	        // Pk, Property Descriptor
+	        // { Value: mappedValue,
+	        //   Writable: true,
+	        //   Enumerable: true,
 	        //   Configurable: true },
 	        // and false.
 
 	        // In browsers that support Object.defineProperty, use the following:
-	        // Object.defineProperty(A, k, { 
-	        //   value: mappedValue, 
-	        //   writable: true, 
-	        //   enumerable: true, 
-	        //   configurable: true 
+	        // Object.defineProperty(A, k, {
+	        //   value: mappedValue,
+	        //   writable: true,
+	        //   enumerable: true,
+	        //   configurable: true
 	        // });
 
 	        // For best browser support, use the following:
@@ -198,7 +198,7 @@ function preventCompatibilityIssues(){
 	    return A;
 	  };
 	}
-	
+
 	// Polyfill for Array function foreach() - from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach#Polyfill
 	// Production steps of ECMA-262, Edition 5, 15.4.4.18
 	// Reference: http://es5.github.io/#x15.4.4.18
@@ -258,7 +258,7 @@ function preventCompatibilityIssues(){
 	    // 8. return undefined
 	  };
 	}
-	
+
 	// Polyfill for Object.keys()
 	// From https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
 	if (!Object.keys) {
