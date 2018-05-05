a81552c3147a1a847cbe051d2089b4f5fc25ff57
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-05-05 15:15:46 -0700

    Metric Model init

---
+++ src/js/models/MetricModel.js
@@ -0,0 +1,23 @@
+/*global define */
+define(['jquery', 'underscore', 'backbone'],
+    function($, _, Backbone) {
+    'use strict';
+
+    // Metric Model 
+    // -------------
+    var Metric = Backbone.Model.extend({
+        defaults: {
+            
+        }
+
+        initialize: function() {
+            
+        }
+
+        getMetricData: function() {
+            
+        }
+        
+    });
+    return Metric;
+});
