bc9b152436d5712843dddb23074ad6e8f61c8db0
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-05-11 13:34:13 -0700

    Metrics in a badge - Bootstrap
    Changes into the CSS and displaying emtric counts as a badge!

--- src/js/views/MetricView.js
+++ src/js/views/MetricView.js
@@ -6,11 +6,14 @@ define(['jquery', 'underscore', 'backbone', 'models/MetricModel', 'views/MetricM
     var MetricView = Backbone.View.extend({
 
         tagName: 'a',
-        className: 'btn metrics',
+        className: 'btn metrics ',
 
         //Templates
-        metricButtonTemplate:  _.template("<span class='value'> <i class='icon icon-spinner icon-spin'></i> </span>" + 
-                            " <i class='icon <%=metricIcon%>'></i> <%=metricName%>" ),
+        metricButtonTemplate:  _.template( "<span class='metric-icon'> <i class='icon" + 
+                            " <%=metricIcon%>'></i> </span>" +
+                            "<span class='metric-name'> <%=metricName%> </span>" +
+                            "<span class='metric-value'> <i class='icon metric-icon icon-spinner icon-spin'>" +
+                            "</i> </span>"),
 
         events: {
 
@@ -39,14 +42,16 @@ define(['jquery', 'underscore', 'backbone', 'models/MetricModel', 'views/MetricM
 
             // Adding tool-tip for the buttons
             // TODO: Change to 'Show metricName', once you've the modals working.
-            this.$el.addClass("tooltip-this")
-                    .attr("data-title", "Dataset " + this.model.get("metricName"))
-                    .attr("data-placement", "top")
-                    .attr("data-trigger", "hover")
-                    .attr("data-container", "body");
+            if (MetacatUI.appModel.get("displayDatasetMetricsTooltip")) {
+                this.$el.addClass("tooltip-this")
+                        .attr("data-title", "Dataset " + this.model.get("metricName"))
+                        .attr("data-placement", "top")
+                        .attr("data-trigger", "hover")
+                        .attr("data-container", "body");
+            };
 
             // waiting for the fetch() call to succeed.
-            this.listenTo(this.model, "change:results", this.renderResults);
+            this.listenTo(this.model, "change:metricRequest", this.renderResults);
             
             this.getMetrics();
             
@@ -61,19 +66,20 @@ define(['jquery', 'underscore', 'backbone', 'models/MetricModel', 'views/MetricM
 
         renderResults: function() {
             var metric = this.model.get('metricName');
-            var results = this.model.get('results');
+            var results = this.model.get(metric.toLowerCase());
 
             // Check if the metric object exists in results obtained from the service 
             // If it does, get its total value else set the total count to 0
-            if(metric.toLowerCase() in results) {
-                var total = results[metric.toLowerCase()].reduce(function(acc, val) { return acc + val; });
+            if(results != null) {
+                var total = results.reduce(function(acc, val) { return acc + val; });
                 this.model.set('metricValue', total);
             } else {
                 this.model.set('metricValue', 0);
             };
 
             // Replacing the metric total count with the spinning icon.
-            this.$('.value').text(total);
+            this.$('.metric-value').addClass("badge")
+            this.$('.metric-value').text(total);
         }
 
     });
