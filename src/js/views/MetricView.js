2b652185a79b799768df71ee98e366a090d98145
Rushiraj Nenuji <rushirajnenuji@gmail.com>
2018-06-18 16:06:20 -0700

    Metrics Number Abbreviator
    Reference #598

--- src/js/views/MetricView.js
+++ src/js/views/MetricView.js
@@ -58,7 +58,6 @@ define(['jquery', 'underscore', 'backbone',  'views/MetricModalView'],
             return this;
         },
 
-
         renderResults: function() {
             var metric = this.metricName
             var results = this.model.get(metric.toLowerCase());
@@ -93,7 +92,38 @@ define(['jquery', 'underscore', 'backbone',  'views/MetricModalView'],
 
             // Replacing the metric total count with the spinning icon.
             this.$('.metric-value').addClass("badge")
-            this.$('.metric-value').text(total);
+            this.$('.metric-value').text(this.numberAbbreviator(total, 1));
+        },
+        
+        numberAbbreviator: function(number, decimalPlaces) {
+            decimalPlaces = Math.pow(10,decimalPlaces);
+            var abbreviations = [ "K", "M", "B", "T" ];
+
+            // Go through the array backwards, so we do the largest first
+            for (var i=abbreviations.length-1; i>=0; i--) {
+
+                // Convert array index to "1000", "1000000", etc
+                var size = Math.pow(10,(i+1)*3);
+
+                // If the number is bigger or equal do the abbreviation
+                if(size <= number) {
+
+                    // Here, we multiply by decimalPlaces, round, and then divide by decimalPlaces.
+                    // This gives us nice rounding to a particular decimal place.
+                    number = Math.round(number*decimalPlaces/size)/decimalPlaces;
+
+                    // Handle special case where we round up to the next abbreviation
+                    if((number == 1000) && (i < abbreviations.length - 1)) {
+                        number = 1;
+                        i++;
+                    }
+
+                    // Add the letter for the abbreviation
+                    number += abbreviations[i];
+                    break;
+                }
+            }
+            return number;
         }
 
     });
