1314807219880c06a06cb408b656365be2d88f7f
jkibele <jkibele@gmail.com>
2018-10-10 16:52:56 -0700

    Placeholder search filtering in CollectionModel.
    Commiting minor changes before working on making ProjectModel a subclass of CollectionModel

--- src/js/models/CollectionModel.js
+++ src/js/models/CollectionModel.js
@@ -33,9 +33,11 @@ define(['jquery', 'underscore', 'backbone', 'models/Search', 'collections/SolrRe
 
       //Need to iterate through the list of filter groups, extract each filter,
       // then field and value for each filter
-
+     
       //Placeholder for now for SASAP
-      this.get("searchModel").set("project", "State of Alaska's Salmon and People");
+      // this.get("searchModel").set("project", "State of Alaska's Salmon and People");
+
+      this.get("searchModel").set("all", ["SASAP"]);
       this.trigger("change:searchModel");
     },
 
@@ -52,6 +54,13 @@ define(['jquery', 'underscore', 'backbone', 'models/Search', 'collections/SolrRe
       //Add the authorization header and other AJAX settings
       requestSettings = _.extend(requestSettings, MetacatUI.appUserModel.createAjaxSettings());
       return Backbone.Model.prototype.fetch.call(this, requestSettings);
+    },
+
+    parse: function(json){
+      // move filter set up stuff here
+      // extract and set search model
+
+      return json
     }
 	});
 
