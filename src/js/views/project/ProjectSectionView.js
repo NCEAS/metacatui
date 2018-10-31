5a0a70a817a5ff6fd905384f23666418827c15c4
Christopher Jones <csj@christopherjones.co>
2018-10-31 16:51:56 -0600

    Stub out the Project Section View
    Add the ProjectSectionView view with stubbed out methods. This view represents a generic tabbed section in the project view.
    
    refs https://github.com/NCEAS/metacatui/issues/815

---
+++ src/js/views/project/ProjectSectionView.js
@@ -0,0 +1,46 @@
+define(['jquery', 
+    'underscore', 
+    'Backbone',
+    'text!templates/project/project-section.html'], function($, _, Backbone, ProjectSectionTemplate){
+    
+    /* The ProjectSectionView is a generic view to render
+     * project sections, with a default rendering of a 
+     * MarkdownView
+     */
+     var ProjectSectionView = Backbone.View.extend({
+        
+        /* The Project Section Element*/
+        el: "#project-section",
+        
+        /* TODO: Decide if we need this */
+        type: "ProjectSection",
+        
+        /* The list of subview instances contained in this view*/
+        subviews: [], // Could be a literal object {}
+        
+        /* Renders the compiled template into HTML */
+        template: _.template(ProjectSectionTemplate),
+        
+        /* The events that this view listens to*/
+        events: {
+            
+        },
+        
+        /* Construct a new instance of ProjectSectionView */
+        initialize: function() {
+            
+        },
+        
+        /* Render the view */
+        render: function() {
+            
+        },
+        
+        onClose: function() {
+            
+        }
+        
+     });
+          
+     return ProjectSectionView;
+});
