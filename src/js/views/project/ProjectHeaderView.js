d87f50180087e325056f4c08fd9cdc3c4e1c500d
Robyn <thiessenbock@nceas.ucsb.edu>
2018-10-31 16:12:46 -0700

    Created ProjectHeaderView
    Created the basic elements of a project header view, including the view and template.
    
    Issue #816

---
+++ src/js/views/project/ProjectHeaderView.js
@@ -0,0 +1,43 @@
+define(["jquery",
+    "underscore",
+    "Backbone",
+    "text!templates/project/projectHeader.html"], function($, _, Backbone, ProjectHeaderTemplate){
+
+    /* The ProjectHeaderView is the view at the top of project pages,
+     * that shows the project's title, synopsis, and logo
+     */
+     var ProjectHeaderView = Backbone.View.extend({
+
+        /* The Project Header Element*/
+        el: "#ProjectHeader",
+
+        /* TODO: Decide if we need this */
+        type: "ProjectHeader",
+
+        /* Renders the compiled template into HTML */
+        template: _.template(ProjectHeaderTemplate),
+
+        /* The events that this view listens to*/
+        events: {
+
+        },
+
+        /* Construct a new instance of ProjectHeaderView */
+        initialize: function() {
+
+        },
+
+        /* Render the view */
+        render: function() {
+
+        },
+
+        /* Close and destroy the view */
+        onClose: function() {
+
+        }
+
+     });
+
+     return ProjectHeaderView;
+});
