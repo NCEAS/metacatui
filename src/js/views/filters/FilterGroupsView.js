d05286fc7f779a3e46611dbbbe556ca929b02e15
laurenwalker <walker@nceas.ucsb.edu>
2019-01-15 18:53:23 -0500

    Display the filters that are applied to the query in the UI.
    Related to #859

--- src/js/views/filters/FilterGroupsView.js
+++ src/js/views/filters/FilterGroupsView.js
@@ -15,6 +15,10 @@ define(['jquery', 'underscore', 'backbone',
 
     className: "filter-groups tabbable",
 
+    events: {
+      "click .remove-filter" : "removeFilter"
+    },
+
     initialize: function (options) {
 
       if( !options || typeof options != "object" ){
@@ -114,6 +118,140 @@ define(['jquery', 'underscore', 'backbone',
         }
 
       });
+
+      this.$el.prepend( $(document.createElement("div")).addClass("filters-header") );
+
+      this.renderAppliedFilters();
+
+    },
+
+    /*
+    * Renders the section of the view that will display the currently-applied filters
+    */
+    renderAppliedFilters: function(){
+
+      var appliedFiltersEl = $(document.createElement("ul")).addClass("applied-filters");
+
+      this.$(".filters-header").append(appliedFiltersEl);
+
+      _.each( this.filterGroups, function(filterGroup){
+
+        var filters = filterGroup.get("filters");
+        this.listenTo(filters, "change:values", this.updateAppliedFilters);
+
+        var dateFilters = _.findWhere(filters, { type: "DateFilter" });
+        this.listenTo(dateFilters, "change:min change:max", this.updateAppliedDateFilters);
+
+      }, this);
+
+    },
+
+    /*
+    * Renders the values of the given Filter Model in the current filter model
+    *
+    * @param {Filter} - The FilterModel to display
+    */
+    updateAppliedFilters: function(filterModel){
+
+      //If the values attribue has changed...
+      if( filterModel.changed && filterModel.changed.values ){
+
+        //Get the new values and the previous values
+        var newValues = filterModel.changed.values,
+            previousValues = filterModel.previousAttributes().values,
+            //Find the values that were removed
+            removedValues  = _.difference(previousValues, newValues),
+            //Find the values that were added
+            addedValues    = _.difference(newValues, previousValues);
+
+        //If a filter has been added, display it
+        _.each(addedValues, function(value){
+
+          //Create the filter label
+          var filterLabel = value;
+
+          //If the filter type is Choice, get the choice label which can be different from the value
+          if( filterModel.type == "ChoiceFilter" ){
+            //Find the choice object with the given value
+            var matchingChoice = _.findWhere(filterModel.get("choices"), { "value" : value });
+
+            //Get the label for that choice
+            if(matchingChoice)
+              filterLabel = matchingChoice.label;
+
+            //If there is no label, default to the value
+            if( !filterLabel )
+              filterLabel = value;
+          }
+
+          //Create the applied filter element
+          var removeIcon    = $(document.createElement("a"))
+                                .addClass("icon icon-remove remove-filter icon-on-right")
+                                .attr("title", "Remove this filter"),
+              appliedFilter = $(document.createElement("li"))
+                                .addClass("applied-filter label")
+                                .text(filterLabel)
+                                .append(removeIcon)
+                                .data("model", filterModel)
+                                .attr("data-value", value);
+
+          //Add the applied filter to the view
+          this.$(".applied-filters").append(appliedFilter);
+
+        }, this);
+
+        //Iterate over each removed filter value and remove them
+        _.each(removedValues, function(value){
+
+          //Find all applied filter elements with a matching value
+          var matchingFilters = this.$(".applied-filter[data-value='" + value + "']");
+
+          //Iterate over each filter element with a matching value
+          _.each(matchingFilters, function(matchingFilter){
+
+            //If this is the filter element associated with this filter model, then remove it
+            if( $(matchingFilter).data("model") == filterModel ){
+              $(matchingFilter).remove();
+            }
+
+          });
+
+        }, this);
+
+      }
+
+    },
+
+    /*
+    * When a DateFilter model is changed, update the applied filters in the UI
+    */
+    updateAppliedDateFilters: function(filterModel){
+
+    },
+
+    /*
+    * Remove the filter from the UI and the Search collection
+    *
+    * @param {Event} - The DOM Event that occured on the filter remove icon
+    */
+    removeFilter: function(e){
+
+      //Get the applied filter element and the filter model associated with it
+      var appliedFilterEl = $(e.target).parents(".applied-filter"),
+          filterModel =  appliedFilterEl.data("model");
+
+      //Remove the given value from the filter model
+      if( filterModel ){
+
+        //Get the current value
+        var values = filterModel.get("values"),
+            //Remove the value that was in this applied filter
+            newValues = _.without(values, appliedFilterEl.data("value"));
+
+        //Updates the values on the model
+        filterModel.set("values", newValues);
+
+      }
 
     }
 
