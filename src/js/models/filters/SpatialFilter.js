dd467c4d5cf390d87a493e2f19c8a3d9af6f4986
Christopher Jones <csj@christopherjones.co>
2019-01-16 13:56:18 -0700

    Add the groupGeohashes method
    Group tiles of 32 subtiles into a group that effectively represents a 
    full parent tile, and group geohashes that fall short of a full tile as 
    well. 
    
    refs #846

--- src/js/models/filters/SpatialFilter.js
+++ src/js/models/filters/SpatialFilter.js
@@ -52,10 +52,39 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
             },
             
             /**
-             *  Consolidate 
+             *  Consolidate geohashes into a parent tile if all are present
              */
             groupGeohashes: function() {
+                var geohashGroups = {};
+                var sortedGeohashes = this.get("geohashes").sort();
+                var groupedGeohashes = _.groupBy(sortedGeohashes, function(geohash) {
+                    return geohash.substring(0, geohash.length - 1);
+                });
+                //Find groups of geohashes that makeup a complete geohash tile (32) 
+                // so we can shorten the query
+                var completeGroups = _.filter(Object.keys(groupedGeohashes), function(group) {
+                    return groupedGeohashes[group].length == 32;
+                });
                 
+                // Find groups that fall short of 32 tiles
+                var incompleteGroups = [];
+                _.each(
+                    _.filter(Object.keys(groupedGeohashes), function(group) { 
+                        return (groupedGeohashes[group].length < 32) 
+                    }), function(incomplete) { 
+                        incompleteGroups.push(groupedGeohashes[n]); 
+                    }
+                );
+                incompleteGroups = _.flatten(incompleteGroups);
+                
+                // Add both complete and incomplete groups to the instance property
+                if((typeof incompleteGroups !== "undefined") && (incompleteGroups.length > 0)) {
+                    geohashGroups[incompleteGroups[0].length.toString()] = incompleteGroups;
+                }
+                if((typeof completeGroups !== "undefined") && (completeGroups.length > 0)) {
+                    geohashGroups[completeGroups[0].length.toString()] = completeGroups;
+                }
+                this.set("geohashGroups", geohashGroups); // Triggers a change event
             }
         });
         return SpatialFilter;
