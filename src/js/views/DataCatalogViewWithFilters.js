1eeb788d7ebf7975223ccc198e1ddec588ae9cbb
Christopher Jones <csj@christopherjones.co>
2019-01-18 14:34:51 -0700

    Minor formatting and documentation changes.

--- src/js/views/DataCatalogViewWithFilters.js
+++ src/js/views/DataCatalogViewWithFilters.js
@@ -1,31 +1,35 @@
 define(["jquery",
-    "underscore",
-    "backbone",
-    "views/DataCatalogView",
-    "text!templates/datacatalog.html"],
-    function($, _, Backbone, DataCatalogView, template){
+        "underscore",
+        "backbone",
+        "views/DataCatalogView",
+        "text!templates/datacatalog.html"
+    ],
+    function($, _, Backbone, DataCatalogView, template) {
 
-    var DataCatalogViewWithFilters = DataCatalogView.extend({
+        /**
+         * A DataCatalogView that uses the Search collection
+         * and the Filter models for managing queries rather than the
+         * Search model and the filter literal objects used in the
+         * parent DataCatalogView.  This accommodates custom project filters.
+         */
+        var DataCatalogViewWithFilters = DataCatalogView.extend({
 
-      template: _.template(template),
+            template: _.template(template),
 
-      /*
-      * Either hides or shows the "clear all filters" button
-      */
-      toggleClearButton: function(){
+            /*
+             * Either hides or shows the "clear all filters" button
+             */
+            toggleClearButton: function() {
 
-        var currentFilters = this.searchModel.getCurrentFilters();
+                var currentFilters = this.searchModel.getCurrentFilters();
 
-        if(currentFilters && currentFilters.length > 0){
-          this.showClearButton();
-        }
-        else{
-          this.hideClearButton();
-        }
-      }
+                if (currentFilters && currentFilters.length > 0) {
+                    this.showClearButton();
+                } else {
+                    this.hideClearButton();
+                }
+            }
+        });
+        return DataCatalogViewWithFilters;
 
     });
-
-    return DataCatalogViewWithFilters;
-
-});
