7bb33d5533e7bfabe9411bca43b73be57442162e
Bryce Mecum <petridish@gmail.com>
2019-04-15 17:15:14 -0700

    Tidy up some minor issues with AnnotationView
    - Tidy up view with better comments
    - Uses the mouse pointer for annotations to show they're clickable
    - Changes the popover direction to bottom which fixes and issue when updaing popover content

--- src/js/views/AnnotationView.js
+++ src/js/views/AnnotationView.js
@@ -17,9 +17,13 @@ define(['jquery',
         termURI: null,
         termLabel: null,
         termDefinition: null,
-        popoverSource: null,
-        visible: null,
-        loaded: null,
+        popoverSource: null, // Stores a reference to the child .annotation el
+                             // which is handy but I'm no sure it's needed
+        visible: null,       // Helps track visibility of the popover so we know
+                             // when it's safe to destroy it and update it with
+                             // new content
+        resolved: null,      // Stores whether we successfully looked and did or
+                             // did not find the definition of the annotaiton
 
         events: {
             "click" : "toggle"
@@ -28,8 +32,7 @@ define(['jquery',
         initialize: function (options) {
             this.termURI = options.termURI;
             this.termLabel = options.termLabel;
-            this.termDefinition = "";
-
+            this.termDefinition = ""; // Fulfilled later via this.query()
             this.visible = false;
             this.resolved = false; // Whether we queried for more details,
                                    // successful or not
@@ -58,7 +61,7 @@ define(['jquery',
             // normal behavior, since we're overriding the normal click
             // behavior in order to edit the popover after we create and show
             // it )
-            if (e.target.className !== "annotation") {
+            if ($(e.target).parent().parent().is(this.$el)) {
                 return;
             }
 
@@ -89,12 +92,14 @@ define(['jquery',
                 return;
             }
 
+            // Verify token before moving on
             if (typeof token !== "string") {
                 this.resolved = true;
 
                 return;
             }
 
+            // Query the API and handle the response
             var url = "https://data.bioontology.org/search?q=" +
                 encodeURIComponent(this.termURI) +
                 "&apikey=" +
@@ -103,11 +108,16 @@ define(['jquery',
             $.get(url, function (data) {
                 var match = null;
 
-                // TODO: Make sure to handle not-found case
-                data.collection.filter(function(result) {
-                    if (result["@id"] === viewRef.termURI) {
-                        match = result;
-                    }
+                // Verify response structure before trusting it
+                if (!data.collection ||
+                    !data.collection.length ||
+                    !data.collection.length > 0) {
+                    return;
+                }
+
+                // Find the first match by URI
+                match = _.find(data.collection, function(result) {
+                    return result["@id"] && result["@id"] === viewRef.termURI;
                 });
 
                 // Verify structure of response looks right and bail out if it
@@ -132,12 +142,20 @@ define(['jquery',
             });
         },
 
-        // Note: Has a side-effect of updating this.popoverSource;
+        /**
+         * Create the Popover for the annotation
+         *
+         * Note: Has a side-effect of updating this.popoverSource;
+         *
+         * @param {string} content - Optional. HTML content for the popover. If
+         * omitted, uses Popover uses the `data-content` attribute on the
+         * annotation source element. If provided, overrides.
+         */
         createPopover: function (content) {
             var el = $(this.$el).children("span").first();
             var options = {
                 trigger: "click",
-                placement: "right",
+                placement: "bottom",
                 container: this.$el,
                 title: this.termLabel,
                 html: true
@@ -149,6 +167,28 @@ define(['jquery',
             options.content = content;
 
             this.popoverSource = el.popover(options);
+        },
+
+        /**
+         * Update the existing Popover for this annotation
+         *
+         * @param {*} popover_content - A JQuery selector or something that can
+         * be passed into a JQuery selector to get the container element for the
+         * popover's content
+         * @param {string} definition - The definition of the annotation's URI
+         */
+        updatePopover: function(popover_content, definition) {
+            // Render updated popover content
+            var new_content = this.bioportalTooltipTemplate({
+                definition: definition,
+                termURI: this.termURI
+            });
+
+            // Update both the existing DOM and the underlying data
+            // attribute in order to persist the updated content between
+            // displays of the popover
+            $(this.popoverSource).data("content", new_content);
+            $(popover_content).html(new_content);
         }
     });
 
