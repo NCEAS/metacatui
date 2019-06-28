a6c1d9ea9ec6bb83073b582c58166ac45db9dfdb
Bryce Mecum <petridish@gmail.com>
2019-06-27 20:55:06 -0800

    Refactor AnnotationView
    Major changes include... (this is not fully done)
    
    - AnnotationVIew now handles the property and value simultaneously, where they used to be one view for each of the property and the value. This matches mockups we decided on together and altogether makes annotations make more sense
    - Wayyyy simpler code. I figured out some stuff wrt Popovers that made my life a lot easier
    - The popover content is now backed by an Underscore template because it was getting messy

--- src/js/views/AnnotationView.js
+++ src/js/views/AnnotationView.js
@@ -2,106 +2,125 @@
 define(['jquery',
     'underscore',
     'backbone',
-    'MetricsChart',
-    'text!templates/metricModalTemplate.html',
-    'collections/Citations',
-    'views/CitationListView'],
-    function($, _, Backbone) {
+    'text!templates/bioportalAnnotationTemplate.html',],
+    function($, _, Backbone, AnnotationPopoverTemplate) {
     'use strict';
 
     var AnnotationView = Backbone.View.extend({
         className: 'annotation-view',
-        template: _.template('<span class="annotation" title="<%= termLabel %>" data-content="<%= termURI %>"><%= termLabel %></span>'),
-        bioportalTooltipTemplate: _.template('<strong>Definition:</strong> <%= definition %><br /><a href="<%= termURI %>" target="_blank"><%= termURI %></a>'),
 
-        termURI: null,
-        termLabel: null,
-        termDefinition: null,
-        popoverSource: null, // Stores a reference to the child .annotation el
-                             // which is handy but I'm no sure it's needed
-        visible: null,       // Helps track visibility of the popover so we know
-                             // when it's safe to destroy it and update it with
-                             // new content
-        resolved: null,      // Stores whether we successfully looked and did or
-                             // did not find the definition of the annotaiton
+        // TODO: Consider removing this and just using the underlying HTML as the template
+        template: _.template('<div class="annotation" title="<%= propertyLabel %> <%= valueLabel %>" data-content=""><div class="annotation-property"><%= propertyLabel %></div><div class="annotation-value"><%= valueLabel %></div></span>'),
+
+        // TODO: XSS protect
+        // TODO: Consider adding a template for this
+        annotationPopoverTemplate: _.template(AnnotationPopoverTemplate),
+
+        el: null,
+        context: null,
+        propertyLabel: null,
+        propertyURI: null,
+        propertyDefinition: null,
+        properyOntology: null,
+        valueLabel: null,
+        valueURI: null,
+        valueDefinition: null,
+        valueOntology: null,
+
+        // Stores a reference to the child .annotation el which is handy but
+        // I'm no sure it's needed
+        // TODO: Rename to popoverReference or something or remove entirley
+        popoverSource: null,
+
+        // Helps track visibility of the popover so we know when it's safe to
+        // destroy it and update it with new content
+        visible: null,
+
+        // Stores whether we successfully looked and did or did not find the
+        // definition of the annotation's property or label, either from cache
+        // or from Bioportal
+        propertyResolved: null,
+        valueResolved: null,
 
         events: {
-            "click" : "toggle"
+            "click" : "togglePopover"
         },
 
-        initialize: function (options) {
-            this.termURI = options.termURI;
-            this.termLabel = options.termLabel;
-            this.termDefinition = ""; // Fulfilled later via this.query()
-            this.visible = false;
-            this.resolved = false; // Whether we queried for more details,
-                                   // successful or not
+        initialize: function () {
+            this.context = this.$el.data('context') || "CONTEXT";
+            this.propertyLabel = this.$el.data('propertyLabel');
+            this.propertyURI = this.$el.data('propertyUri');
+            this.valueLabel = this.$el.data('valueLabel');
+            this.valueURI = this.$el.data('valueUri');
+
+            // TODO: Not sure if I have to have these
+            this.valueDefinition = null;
+            this.valueOntology = null;
+
+            this.propertyResolved = false;
+            this.valueResolved = false;
         },
 
         render: function () {
-            this.$el.html(this.template({
-                termLabel: this.termLabel,
-                termURI: this.termURI,
-                termDefinition: this.termDefinition
-            }));
-
-            this.createPopover(
-                '<a href="' + this.termURI + '" target="_blank">' +
-                this.termURI +
-                '</a>'
-            );
+            this.createPopover();
 
             return this;
         },
 
         // Destroy the popover if it's visible, stale
-        toggle: function (e) {
-            // Don't do anything if we clicked on the popover
-            // (This lets people click and copy/paste on the popover text, aka
-            // normal behavior, since we're overriding the normal click
-            // behavior in order to edit the popover after we create and show
-            // it )
-            if ($(e.target).parent().parent().is(this.$el)) {
+        // Allows the user to copy and paste text from popovers
+        togglePopover: function (e) {
+            this.queryAndUpdateProperty();
+            this.queryAndUpdateValue();
+        },
+
+        /* Either look up from cache or query Bioportal to find definitions for
+        the annotation property and value and then update the tooltip */
+        queryAndUpdateProperty: function () {
+            if (this.propertyResolved) {
                 return;
             }
 
-            if (!this.resolved) {
-                this.query();
-            }
-
-            if (this.visible) {
-                // Re-create the popover
-                this.popoverSource.popover("destroy");
-                this.createPopover();
-            }
-
-            this.visible = !this.visible;
-        },
-
-        query: function () {
             var viewRef = this,
-                popover_content = $(this.$el).find(".popover-content").first(),
                 cache = MetacatUI.appModel.get("bioportalLookupCache"),
                 token = MetacatUI.appModel.get("bioportalAPIKey");
 
             // Attempt to grab from cache first
-            if (cache && cache[this.termURI]) {
-                this.updatePopover(popover_content, cache[this.termURI]);
-                this.resolved = true;
+
+            // TODO: Update for new popover model
+            if (cache && cache[this.propertyURI]) {
+                this.propertyDefinition = cache[this.propertyURI].definition;
+                this.propertyOntology = cache[this.propertyURI].links.ontology;
+                this.updatePopover();
+                this.propertyResolved = true;
 
                 return;
             }
 
             // Verify token before moving on
             if (typeof token !== "string") {
-                this.resolved = true;
+                this.propertyResolved = true;
 
                 return;
             }
 
+            // // Temporary to avoid hitting the API
+            // setTimeout(function() {
+            //     viewRef.propertyDefinition = "There's a voice that keeps on calling me. Down the road, that's where I'll always be. Every stop I make, I make a new friend."
+            //     viewRef.propertyOntology = "https://example.org/ontology"
+            //     viewRef.updatePopover();
+            //     viewRef.propertyResolved = true;
+            // }, 500);
+
+
+            // return;
+
+            // End temporary
+
             // Query the API and handle the response
+            // TODO: Looks like we should proxy this so the token doesn't leak
             var url = "https://data.bioontology.org/search?q=" +
-                encodeURIComponent(this.termURI) +
+                encodeURIComponent(this.propertyURI) +
                 "&apikey=" +
                 token;
 
@@ -117,7 +136,7 @@ define(['jquery',
 
                 // Find the first match by URI
                 match = _.find(data.collection, function(result) {
-                    return result["@id"] && result["@id"] === viewRef.termURI;
+                    return result["@id"] && result["@id"] === viewRef.propertyURI;
                 });
 
                 // Verify structure of response looks right and bail out if it
@@ -126,19 +145,101 @@ define(['jquery',
                     !match.definition ||
                     !match.definition.length ||
                     !match.definition.length > 0) {
-                    viewRef.resolved = true;
+                    viewRef.propertyResolved = true;
 
                     return;
                 }
 
-                viewRef.updatePopover(popover_content, match.definition[0]);
+                viewRef.propertyDefinition = match.definition[0];
+                viewRef.propertyOntology = match.links.ontology;
+                viewRef.updatePopover();
+                viewRef.updateCache(viewRef.propertyURI, match);
 
-                // Cache the result for next time
-                if (cache) {
-                    cache[viewRef.termURI] = match.definition[0];
+                viewRef.propertyResolved = true;
+            });
+        },
+
+        queryAndUpdateValue: function () {
+            if (this.valueResolved) {
+                return;
+            }
+
+            var viewRef = this,
+                cache = MetacatUI.appModel.get("bioportalLookupCache"),
+                token = MetacatUI.appModel.get("bioportalAPIKey");
+
+            // Attempt to grab from cache first
+
+            // TODO: Update for new popover model
+            if (cache && cache[this.valueURI]) {
+                this.valueDefinition = cache[this.valueURI].definition;
+                this.valueOntology = cache[this.valueURI].links.ontology;
+                this.updatePopover();
+                this.valueResolved = true;
+
+                return;
+            }
+
+            // Verify token before moving on
+            if (typeof token !== "string") {
+                this.valueResolved = true;
+
+                return;
+            }
+
+
+            // // Temporary to avoid hitting the API
+            // setTimeout(function() {
+            //     viewRef.valueDefinition = "There's a voice that keeps on calling me. Down the road, that's where I'll always be. Every stop I make, I make a new friend."
+            //     viewRef.valueOntology = "https://example.org/ontology"
+            //     viewRef.updatePopover();
+            //     viewRef.valueResolved = true;
+            // }, 500);
+
+            // return;
+
+
+
+
+            // Query the API and handle the response
+            // TODO: Looks like we should proxy this so the token doesn't leak
+            var url = "https://data.bioontology.org/search?q=" +
+                encodeURIComponent(this.valueURI) +
+                "&apikey=" +
+                token;
+
+            $.get(url, function (data) {
+                var match = null;
+
+                // Verify response structure before trusting it
+                if (!data.collection ||
+                    !data.collection.length ||
+                    !data.collection.length > 0) {
+                    return;
                 }
 
-                viewRef.resolved = true;
+                // Find the first match by URI
+                match = _.find(data.collection, function(result) {
+                    return result["@id"] && result["@id"] === viewRef.valueURI;
+                });
+
+                // Verify structure of response looks right and bail out if it
+                // doesn't
+                if (!match ||
+                    !match.definition ||
+                    !match.definition.length ||
+                    !match.definition.length > 0) {
+                    viewRef.valueResolved = true;
+
+                    return;
+                }
+
+                viewRef.valueDefinition = match.definition[0];
+                viewRef.valueOntology = match.links.ontology;
+
+                viewRef.updatePopover();
+                viewRef.updateCache(viewRef.valueURI, match);
+                viewRef.valueResolved = true;
             });
         },
 
@@ -146,51 +247,61 @@ define(['jquery',
          * Create the Popover for the annotation
          *
          * Note: Has a side-effect of updating this.popoverSource;
-         *
-         * @param {string} content - Optional. HTML content for the popover. If
-         * omitted, uses Popover uses the `data-content` attribute on the
-         * annotation source element. If provided, overrides.
          */
-        createPopover: function (content) {
-            var el = $(this.$el).children("span").first();
-            var options = {
-                trigger: "click",
-                placement: "bottom",
+        createPopover: function () {
+            var new_content = this.annotationPopoverTemplate({
+                context: this.context,
+                propertyLabel: this.propertyLabel,
+                propertyURI: this.propertyURI,
+                propertyDefinition: this.propertyDefinition,
+                propertyOntology: this.propertyOntology,
+                valueLabel: this.valueLabel,
+                valueURI: this.valueURI,
+                valueDefinition: this.valueDefinition,
+                valueOntology: this.valueOntology
+            });
+
+            $(this.$el).data("content", new_content);
+
+            this.popoverSource = $(this.$el).popover({
                 container: this.$el,
-                title: this.termLabel,
-                html: true
-            };
-
-            // Override the content if provided, allows us to get around an
-            // apparently limitation in underscore related to double quotes
-            // in template strings I couldn't figure out
-            options.content = content;
-
-            this.popoverSource = el.popover(options);
+            });
         },
 
-        /**
-         * Update the existing Popover for this annotation
-         *
-         * @param {*} popover_content - A JQuery selector or something that can
-         * be passed into a JQuery selector to get the container element for the
-         * popover's content
-         * @param {string} definition - The definition of the annotation's URI
-         */
-        updatePopover: function(popover_content, definition) {
-            // Render updated popover content
-            var new_content = this.bioportalTooltipTemplate({
-                definition: definition,
-                termURI: this.termURI
+
+        updatePopover: function() {
+            var popover_content = $(this.$el).find(".popover-content").first();
+
+            var new_content = this.annotationPopoverTemplate({
+                context: this.context,
+                propertyLabel: this.propertyLabel,
+                propertyURI: this.propertyURI,
+                propertyDefinition: this.propertyDefinition,
+                propertyOntology: this.propertyOntology,
+                valueLabel: this.valueLabel,
+                valueURI: this.valueURI,
+                valueDefinition: this.valueDefinition,
+                valueOntology: this.valueOntology
             });
 
             // Update both the existing DOM and the underlying data
             // attribute in order to persist the updated content between
             // displays of the popover
-            $(this.popoverSource).data("content", new_content);
+            $(this.$el).data("content", new_content);
             $(popover_content).html(new_content);
+        },
+
+        /** */
+        updateCache: function(term, match) {
+            var cache = MetacatUI.appModel.get("bioportalLookupCache");
+
+            if (cache &&
+                typeof term === "string" &&
+                typeof match === "string") {
+                cache[term] = match;
+            }
         }
     });
 
-     return AnnotationView;
+    return AnnotationView;
   });
