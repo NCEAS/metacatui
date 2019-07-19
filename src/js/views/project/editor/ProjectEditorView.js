0b35c9373689ebb67b83805d569be2d21285dfa6
Rushiraj Nenuji <nenuji@nceas.ucsb.edu>
2019-07-19 09:16:47 -0700

    Create and fetch() the ProjectModel in the editor
    Reference: #981
    
    The ProjectEditorView listens to the ProjectModel sync event and renders the view on successful sync event.
    
    TODO:
    1) Handle loading of the default Project Model
    2) Handle fetch() fail options.

--- src/js/views/project/editor/ProjectEditorView.js
+++ src/js/views/project/editor/ProjectEditorView.js
@@ -4,8 +4,10 @@ define(['underscore',
         'models/project/ProjectModel',
         'views/EditorView',
         "views/project/editor/ProjEditorSectionsView",
-        "text!templates/project/editor/projectEditor.html"],
-function(_, $, Backbone, Project, EditorView, ProjEditorSectionsView, Template){
+        "text!templates/loading.html",
+        "text!templates/project/editor/projectEditor.html"
+      ],
+function(_, $, Backbone, Project, EditorView, ProjEditorSectionsView, LoadingTemplate, Template){
 
   /**
   * @class ProjectEditorView
@@ -34,6 +36,7 @@ function(_, $, Backbone, Project, EditorView, ProjEditorSectionsView, Template){
     * References to templates for this view. HTML files are converted to Underscore.js templates
     */
     template: _.template(Template),
+    loadingTemplate: _.template(LoadingTemplate),
 
     /**
     * The events this view will listen to and the associated function to call.
@@ -50,12 +53,42 @@ function(_, $, Backbone, Project, EditorView, ProjEditorSectionsView, Template){
     */
     initialize: function(options){
 
+      // initializing the ProjectEditorView properties
+      this.projectIdentifier = options.projectIdentifier ? options.projectIdentifier : undefined;
     },
 
     /**
     * Renders the ProjectEditorView
     */
     render: function(){
+
+      // Display a spinner to indicate loading until model is created.
+      this.$el.html(this.loadingTemplate({
+        msg: "Retrieving project details..."
+      }));
+
+      //Get the model
+      this.createModel();
+
+      // When the model has been synced, render the results
+      this.stopListening();
+      this.listenTo(this.model, "sync", this.renderProjectEditor);
+
+      //Get the model
+      this.model.fetch();
+
+      return this;
+    },
+
+    /**
+    * Renders the project editor view once the project view is created
+    */
+    renderProjectEditor: function() {
+
+      // hide the loading
+      if (this.$loading) {
+        this.$loading.remove();
+      }
 
       //Add the template to the view and give the body the "Editor" class
       this.$el.html(this.template());
@@ -68,10 +101,58 @@ function(_, $, Backbone, Project, EditorView, ProjEditorSectionsView, Template){
     },
 
     /**
-    * Gets the ProjectModel from the repository or creates a new one
+    * Create a ProjectModel object
     */
-    getModel: function(){
+    createModel: function(){
+      
+      /**
+      * The name of the project
+      * @type {string}
+      */
+      var projectName;
 
+      /**
+      * The dictionary object of project names mapped to corresponding identifiers
+      * @type {object}
+      */
+      var projectsMap = MetacatUI.appModel.get("projectsMap");
+      
+      // Look up the project document seriesId by its registered name if given
+      if ( this.projectIdentifier ) {
+        if ( projectsMap ) {
+          
+          // Do a forward lookup by key
+          if ( typeof (projectsMap[this.projectIdentifier] ) !== "undefined" ) {
+            this.projectName = this.projectIdentifier;
+            this.projectIdentifier = projectsMap[this.projectIdentifier];
+          } else { 
+            // Try a reverse lookup of the project name by values
+            this.projectName = _.invert(projectsMap)[this.projectIdentifier];
+
+            if ( typeof this.projectName === "undefined" ) {
+              //Try looking up the project name with case-insensitive matching
+              this.projectName = _.findKey(projectsMap, function(value, key){
+                return( key.toLowerCase() == this.projectIdentifier.toLowerCase() );
+              });
+
+              //If a matching project name was found, get the corresponding identifier
+              if( this.projectName ){
+                //Get the project ID from the map
+                this.projectIdentifier = projectsMap[this.projectName];
+              }
+            }
+          }
+        }
+
+        // Create a new project model with the identifier
+        this.model = new Project({
+          id: this.projectIdentifier
+        });
+
+      } else {
+          // Create a new, default project model
+          this.model = new Project();
+      }
     }
 
   });
