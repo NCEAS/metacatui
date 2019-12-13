a0ef24312df1a468fa0d584382ac6f968be5a186
laurenwalker <walker@nceas.ucsb.edu>
2019-12-13 15:49:15 -0600

    Change version to 2.8.2

--- src/loader.js
+++ src/loader.js
@@ -24,7 +24,7 @@ MetacatUI.root = "/metacatui"
 MetacatUI.root = MetacatUI.root.replace(/\/$/, "");
 
 //This version of Metacat UI - used for cache busting
-MetacatUI.metacatUIVersion = "2.8.1";
+MetacatUI.metacatUIVersion = "2.8.2";
 
 MetacatUI.loadTheme = function(theme) {
     var script = document.createElement("script");
