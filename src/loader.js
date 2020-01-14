1cd14a8b3ef7a5ed68f8cba8f629ad58dab77b4c
laurenwalker <walker@nceas.ucsb.edu>
2020-01-14 15:31:42 -0600

    Added includes() polyfill. #1236

--- src/loader.js
+++ src/loader.js
@@ -381,36 +381,53 @@ MetacatUI.preventCompatibilityIssues = function(){
     if (window.NodeList && !NodeList.prototype.forEach) {
       NodeList.prototype.forEach = Array.prototype.forEach;
     }
-}
 
-if (typeof Object.assign != 'function') {
-  // Must be writable: true, enumerable: false, configurable: true
-  Object.defineProperty(Object, "assign", {
-    value: function assign(target, varArgs) { // .length of function is 2
-      'use strict';
-      if (target == null) { // TypeError if undefined or null
-        throw new TypeError('Cannot convert undefined or null to object');
-      }
+    //Polyfill for Object.assign()
+    if (typeof Object.assign != 'function') {
+      // Must be writable: true, enumerable: false, configurable: true
+      Object.defineProperty(Object, "assign", {
+        value: function assign(target, varArgs) { // .length of function is 2
+          'use strict';
+          if (target == null) { // TypeError if undefined or null
+            throw new TypeError('Cannot convert undefined or null to object');
+          }
 
-      var to = Object(target);
+          var to = Object(target);
 
-      for (var index = 1; index < arguments.length; index++) {
-        var nextSource = arguments[index];
+          for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
 
-        if (nextSource != null) { // Skip over if undefined or null
-          for (var nextKey in nextSource) {
-            // Avoid bugs when hasOwnProperty is shadowed
-            if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
-              to[nextKey] = nextSource[nextKey];
+            if (nextSource != null) { // Skip over if undefined or null
+              for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                  to[nextKey] = nextSource[nextKey];
+                }
+              }
             }
           }
+          return to;
+        },
+        writable: true,
+        configurable: true
+      });
+    }
+
+    //Polyfill for Array.includes, taken from https://stackoverflow.com/questions/31221341/ie-does-not-support-includes-method
+    if (!String.prototype.includes) {
+      String.prototype.includes = function(search, start) {
+        'use strict';
+        if (typeof start !== 'number') {
+          start = 0;
         }
-      }
-      return to;
-    },
-    writable: true,
-    configurable: true
-  });
+
+        if (start + search.length > this.length) {
+          return false;
+        } else {
+          return this.indexOf(search, start) !== -1;
+        }
+      };
+    }
 }
 
 MetacatUI.preventCompatibilityIssues();
