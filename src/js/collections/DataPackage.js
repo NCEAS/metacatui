59b90d9a791f42a3514b2303c31eff1a51c97bac
Bryce Mecum <petridish@gmail.com>
2020-04-07 19:01:52 -0800

    Prevent multiple-saves of data objects
    I think the old logic here was based on the assumption of eager saving when
    items are added to the editor. Now that replacing files delays the save until
    the entire editor is saved, data models need to behave much more like metadata
    models. So data models should only be saved when they have content changes and
    are not in progress, complete, or in error. Note that I could make this
    conditional simpler code-wise but I'm going to leave it as it is because it's
    very clear.

--- src/js/collections/DataPackage.js
+++ src/js/collections/DataPackage.js
@@ -1098,8 +1098,14 @@ define(['jquery', 'underscore', 'backbone', 'rdflib', "uuid", "md5",
               //Models should be saved if they are in the save queue, had an error saving earlier,
               //or they are Science Metadata model that is NOT already in progress
               return (
-                  (m.get("type") == "Metadata" && m.get("uploadStatus") == "q") ||
-                  (m.get("type") == "Data" && m.get("hasContentChanges")) ||
+                  (m.get("type") == "Metadata" &&
+                    m.get("uploadStatus") == "q") ||
+                  (m.get("type") == "Data" &&
+                    m.get("hasContentChanges") &&
+                    m.get("uploadStatus") != "p" &&
+                    m.get("uploadStatus") != "c" &&
+                    m.get("uploadStatus") != "e" &&
+                    m.get("uploadStatus") !== null) ||
                   (m.get("type") == "Metadata" &&
                       m.get("uploadStatus") != "p" &&
                       m.get("uploadStatus") != "c" &&
