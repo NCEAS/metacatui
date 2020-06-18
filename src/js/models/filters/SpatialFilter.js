19a2a60dd260d6b7c1458b89dbde17b5e3ea13fe
laurenwalker <walker@nceas.ucsb.edu>
2020-06-18 17:38:01 -0400

    Added support for saving SpatialFilters to a collection/portal. #1156

--- src/js/models/filters/SpatialFilter.js
+++ src/js/models/filters/SpatialFilter.js
@@ -2,8 +2,8 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
     function(_, $, Backbone, Filter) {
 
         /**
-        *A SpatialFilter represents a spatial constraint on the query to be executed,
-        *and stores the geohash strings for all of the geohash tiles that coincide with the
+        * A SpatialFilter represents a spatial constraint on the query to be executed,
+        * and stores the geohash strings for all of the geohash tiles that coincide with the
         * search bounding box at the given zoom level.
         * @class SpatialFilter
         * @name SpatialFilter
@@ -13,13 +13,14 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
         var SpatialFilter = Filter.extend(
           /** @lends SpatialFilter.prototype */{
 
-            /* The type of the filter */
+            /**
+             @inheritdoc
+            */
             type: "SpatialFilter",
 
             /**
-            * @type {object}
-            * @property {string} icon - The icon to display with the Spatial filter
-            * @property {string} label - A short intuitive label
+            * Inherits all default properties of {@link Filter}
+            * @property {string[]} geohashes
             */
             defaults: function() {
                 return _.extend(Filter.prototype.defaults(), {
@@ -27,7 +28,8 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
                     /* The array of geohashes used to spatially constrain the search*/
                     geohashes: [],
 
-                    /* The easternmost longitude of the represented bounding box */
+                    /**
+                     The easternmost longitude of the represented bounding box */
                     east: null,
 
                     /* The westernmost longitude of the represented bounding box */
@@ -46,13 +48,10 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
                     groupedGeohashes: {},
 
                     label: "Limit search to the map area",
-                    icon: "globe"
+                    icon: "globe",
+                    operator: "OR",
+                    matchSubstring: false
                 });
-            },
-
-            /* Standard events and callbacks for this model */
-            events: {
-                "change geohashes": this.groupGeohashes
             },
 
             /**
@@ -105,7 +104,41 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
             },
 
             /**
-             *  Consolidate geohashes into a parent tile if all are present
+            * @inheritdoc
+            */
+            updateDOM: function(options){
+
+              try{
+                var updatedDOM = Filter.prototype.updateDOM.call(this, options),
+                    $updatedDOM = $(updatedDOM);
+
+                //Force the serialization of the "operator" node for SpatialFilters,
+                // since the Filter model will skip default values
+                var operatorNode = updatedDOM.ownerDocument.createElement("operator");
+                operatorNode.textContent = this.get("operator");
+
+                var matchSubstringNode = updatedDOM.ownerDocument.createElement("matchSubstring");
+                matchSubstringNode.textContent = this.get("matchSubstring");
+
+                //Insert the operator node
+                $updatedDOM.children("field").last().after(operatorNode);
+
+                //Insert the matchSubstring node
+                $(matchSubstringNode).insertBefore($updatedDOM.children("value").first());
+
+                //Return the updated DOM
+                return updatedDOM;
+              }
+              catch(e){
+                console.error("Unable to serialize a SpatialFilter.", e);
+                return this.get("objectDOM") || "";
+              }
+            },
+
+            /**
+             *  Consolidates geohashes into groups based on their geohash level
+             *  and updates the model with those groups. The fields and values attributes
+             *  on this model are also updated with the geohashes.
              */
             groupGeohashes: function() {
                 var geohashGroups = {};
@@ -138,7 +171,26 @@ define(["underscore", "jquery", "backbone", "models/filters/Filter"],
                     geohashGroups[completeGroups[0].length.toString()] = completeGroups;
                 }
                 this.set("geohashGroups", geohashGroups); // Triggers a change event
+
+                //Determine the field and value attributes
+                var fields = [],
+                    values = [];
+                _.each( Object.keys(geohashGroups), function(geohashLevel){
+                  fields.push( "geohash_" + geohashLevel );
+                  values = values.concat( geohashGroups[geohashLevel].slice() );
+                }, this);
+
+                this.set("fields", fields);
+                this.set("values", values);
             },
+
+            /**
+            * @inheritdoc
+            */
+            resetValue: function(){
+              this.set("fields", this.defaults().fields);
+              this.set("values", this.defaults().values);
+            }
         });
         return SpatialFilter;
 });
