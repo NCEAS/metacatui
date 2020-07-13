95d9f3d3ee4373a828bb74382ce6fd0c185965af
laurenwalker <walker@nceas.ucsb.edu>
2020-07-13 18:41:02 -0500

    Fix bug in dataone config

--- src/js/themes/dataone/config.js
+++ src/js/themes/dataone/config.js
@@ -30,7 +30,6 @@ MetacatUI.AppConfig = Object.assign({
   enablePublishDOI: false,
   showMyPortals: false,
   enableCreatePortals: false,
-  defaultSearchFilters: ["all", "attribute", "documents", "dataSource", "creator", "dataYear", "pubYear", "id", "taxon", "spatial"]
   defaultSearchFilters: ["all", "attribute", "annotation", "documents", "dataSource", "creator", "dataYear", "pubYear", "id", "taxon", "spatial"]
 }, MetacatUI.AppConfig);
 
@@ -38,7 +37,7 @@ MetacatUI.themeMap =
 {
 	'*': {
 		// example overrides are provided here
-		'templates/appHead.html' : MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/templates/apphead.html',
+		'templates/appHead.html' : MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/templates/appHead.html',
     'templates/app.html' : MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/templates/app.html',
 		'templates/navbar.html' : MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/components/d1website/navbar.html',
 		'templates/footer.html' : MetacatUI.root + '/js/themes/' + MetacatUI.theme + '/components/d1website/footer.html',
