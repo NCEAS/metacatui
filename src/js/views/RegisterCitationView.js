39e3d47a51d203e9eaff55ee656dfb2d20ae275d
laurenwalker <walker@nceas.ucsb.edu>
2020-07-28 11:30:36 -0400

    Fix some indentation / formatting

--- src/js/views/RegisterCitationView.js
+++ src/js/views/RegisterCitationView.js
@@ -53,9 +53,9 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/registerCitation.htm
         * Hides and removes this view from the page.
         */
         teardown: function() {
-            this.$el.modal('hide');
-            this.$el.data('modal', null);
-            this.remove();
+          this.$el.modal('hide');
+          this.$el.data('modal', null);
+          this.remove();
         },
 
         /**
@@ -73,67 +73,65 @@ define(['jquery', 'underscore', 'backbone', 'text!templates/registerCitation.htm
          */
         registerCitation: function() {
 
-            // get the input values
-            var publicationIdentifier = this.$("#publication-identifier").val();
+          // get the input values
+          var publicationIdentifier = this.$("#publication-identifier").val();
 
-            var citationType = this.$("#citationTypeCustomSelect").val();
-            var relation_type = null;
+          var citationType = this.$("#citationTypeCustomSelect").val();
+          var relation_type = null;
 
-            // If the user has not selected a valid
-            if (citationType != 0) {
-                relation_type = citationType == 1 ? "isCitedBy" : "isReferencedBy";
+          // If the user has not selected a valid
+          if (citationType != 0) {
+              relation_type = citationType == 1 ? "isCitedBy" : "isReferencedBy";
+          }
+          else {
+              relation_type = "isCitedBy";
+          }
+
+          // create a request object
+          var citationsUrl = MetacatUI.appModel.get("d1CitationUrl");
+          this.citationsUrl = citationsUrl;
+
+          // get the form data before replacing everything with the loading icon!
+          var formData = {};
+          formData["request_type"] = "dataset";
+          formData["metadata"] = new Array();
+
+          var citationRegisterObject = {};
+          citationRegisterObject["target_id"] = this.pid;
+          citationRegisterObject["source_id"] = publicationIdentifier;
+          citationRegisterObject["relation_type"] = relation_type;
+          formData["metadata"].push(citationRegisterObject);
+
+          // ajax call to submit the given form and then render the results in the content area
+          var viewRef = this;
+
+          var requestSettings = {
+            type: "POST",
+            url: this.citationsUrl,
+            contentType: false,
+            processData: false,
+            data: JSON.stringify(formData),
+            dataType: "json",
+            success: function(data, textStatus, jqXHR) {
+              var successMessage = $.parseHTML(viewRef.alertTemplate({
+                msg: 'Thank you! Your citation has been successfully submitted to DataONE. It may take upto 24 hours to display the citation.',
+                classes: "alert-success"
+              }));
+
+              viewRef.$(".modal-body").html(successMessage);
+              viewRef.$(".modal-footer").html(viewRef.successFooterTemplate());
+            },
+            error: function(){
+              var errorMessage = $.parseHTML(viewRef.alertTemplate({
+                  msg: 'Sorry! We encountered an error while registering that citation to DataONE.',
+                  classes: "alert-error"
+              }));
+
+              viewRef.$(".modal-body").html(errorMessage);
             }
-            else {
-                relation_type = "isCitedBy";
-            }
+          }
 
-            // create a request object
-            var citationsUrl = MetacatUI.appModel.get("d1CitationUrl");
-            this.citationsUrl = citationsUrl;
-
-            // get the form data before replacing everything with the loading icon!
-            var formData = {};
-            formData["request_type"] = "dataset";
-            formData["metadata"] = new Array();
-
-            var citationRegisterObject = {};
-            citationRegisterObject["target_id"] = this.pid;
-            citationRegisterObject["source_id"] = publicationIdentifier;
-            citationRegisterObject["relation_type"] = relation_type;
-            formData["metadata"].push(citationRegisterObject);
-
-      // ajax call to submit the given form and then render the results in the content area
-      var viewRef = this;
-
-      var requestSettings = {
-          type: "POST",
-          url: this.citationsUrl,
-          contentType: false,
-                    processData: false,
-                    data: JSON.stringify(formData),
-                    dataType: "json",
-          success: function(data, textStatus, jqXHR) {
-            var  successMessage = $.parseHTML(viewRef.alertTemplate({
-                            msg: 'Thank you! Your citation has been successfully submitted to DataONE. It may take upto 24 hours to display the citation.',
-                            classes: "alert-success"
-                        }));
-
-
-                        viewRef.$(".modal-body").html(successMessage);
-                        viewRef.$(".modal-footer").html(viewRef.successFooterTemplate());
-                    },
-                    error: function(){
-                        var  errorMessage = $.parseHTML(viewRef.alertTemplate({
-                            msg: 'Sorry! We encountered an error while registering that citation to DataONE.',
-                            classes: "alert-error"
-                        }));
-
-
-                        viewRef.$(".modal-body").html(errorMessage);
-                    }
-      }
-
-            $.ajax(_.extend(requestSettings, MetacatUI.appUserModel.createAjaxSettings()));
+          $.ajax(_.extend(requestSettings, MetacatUI.appUserModel.createAjaxSettings()));
 
         }
 
