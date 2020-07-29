NCEAS/metacatui

commit b41075da57220782a2311aaab09d5a0577b4ce3f

Fix bug in SearchResultView link to MN profile

src/js/views/SearchResultView.js
@@ -85,14 +85,14 @@ define(['jquery', 'underscore', 'backbone', 'models/SolrResult', 'models/Package
         if( json.logo && !json.logo.startsWith("http") ){
           json.logo = MetacatUI.appModel.get("objectServiceUrl") + json.logo;
         }
-        
+
         var datasourceId = json.memberNode? json.memberNode.identifier : json.datasource,
             currentMemberNode = MetacatUI.appModel.get("nodeId") || datasourceId;
 
         //Construct a URL to the profile of this repository
         json.profileURL = (datasourceId == currentMemberNode)?
                            MetacatUI.root + "/profile" :
-                           MetacatUI.appModel.get("dataoneSearchUrl") + "/portals/" + json.memberNode.shortIdentifier;
+                           MetacatUI.appModel.get("dataoneSearchUrl") + "/portals/" + datasourceId;
 
       }
 

commit 5b5dec6fa4ed22cfc69ebce75abeaf26ed14907a

Fix bug introduced in b41075da57220782a2311aaab09d5a0577b4ce3f

src/js/views/SearchResultView.js
@@ -92,7 +92,7 @@ define(['jquery', 'underscore', 'backbone', 'models/SolrResult', 'models/Package
         //Construct a URL to the profile of this repository
         json.profileURL = (datasourceId == currentMemberNode)?
                            MetacatUI.root + "/profile" :
-                           MetacatUI.appModel.get("dataoneSearchUrl") + "/portals/" + datasourceId;
+                           MetacatUI.appModel.get("dataoneSearchUrl") + "/portals/" + datasourceId.replace("urn:node:", "");
 
       }
 