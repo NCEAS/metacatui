646d20f04d41ac279a5ee8fc835a7e6fb5be836d
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2020-11-30 13:33:17 -0500

    Show error in AccessPolicyView when sysMeta update gives 401 error
    Relates to #1418

--- src/js/views/AccessPolicyView.js
+++ src/js/views/AccessPolicyView.js
@@ -621,7 +621,7 @@ function(_, $, Backbone, AccessRule, AccessPolicy, AccessRuleView, Template, Tog
 
       }
       //When the status is "error"
-      else if( status == "e" ){
+      else if( status == "e" || status == "w" ){
         var msgContainer = this.$(".modal-body").length? this.$(".modal-body") : this.$el;
 
         MetacatUI.appView.showAlert(
