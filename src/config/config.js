32fdac933f996cb999f0df0763093201cf821f55
wykhuh <wykhuh@users.noreply.github.com>
2021-01-28 09:38:11 -0800

    remove settings from config.js

--- src/config/config.js
+++ src/config/config.js
@@ -5,19 +5,5 @@
 * Full list of AppConfig options: https://nceas.github.io/metacatui/docs/AppConfig.html
 */
 MetacatUI.AppConfig = {
-  //The path to the root location of MetacatUI, i.e. where index.html is
-  root: "/",
-  //Turn off the map features for now
-  mapKey: "",
-  //Turn off the Google Analytics
-  googleAnalyticsKey: "",
-  //Use the test.arcticdata.io repository, for development. DO NOT develop against the production arcticdata.io repository.
-  baseUrl: "https://test.arcticdata.io",
-  d1CNBaseUrl: "https://cn-stage.test.dataone.org/",
-  mdqBaseUrl: "https://api.test.dataone.org:30443/quality",
-  dataoneSearchUrl: "https://search-stage.test.dataone.org",
-  //Allow 1000 portals to be created (instead of only ~5)
-  portalLimit: 1000,
-  //Use the Arctic theme
-  theme: "arctic",
+
 }
