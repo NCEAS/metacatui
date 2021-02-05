cf80e2474bd30ae0159b5c0c95987f4b699383ec
Bryce Mecum <petridish@gmail.com>
2021-02-05 10:39:01 -0900

    Remove unused parts of annotation support
    I had built out some pieces to support general annotation editing but we decided to remove them for now so I'm cleaning up.
    
    Ref #1402

--- src/js/views/metadata/EMLAttributeView.js
+++ src/js/views/metadata/EMLAttributeView.js
@@ -2,14 +2,12 @@
 define(['underscore', 'jquery', 'backbone',
         'models/DataONEObject',
         'models/metadata/eml211/EMLAttribute',
-        'models/metadata/eml211/EMLAnnotation',
         'models/metadata/eml211/EMLMeasurementScale',
-        'views/metadata/EMLAnnotationView',
         'views/metadata/EMLMeasurementScaleView',
         'views/metadata/EMLMeasurementTypeView',
         'text!templates/metadata/eml-attribute.html'],
-		function(_, $, Backbone, DataONEObject, EMLAttribute, EMLAnnotation,
-			EMLMeasurementScale, EMLAnnotationView, EMLMeasurementScaleView, EMLMeasurementTypeView, EMLAttributeTemplate){
+		function(_, $, Backbone, DataONEObject, EMLAttribute,
+			EMLMeasurementScale, EMLMeasurementScaleView, EMLMeasurementTypeView, EMLAttributeTemplate){
 
         /**
         * @class EMLAttributeView
@@ -36,9 +34,7 @@ define(['underscore', 'jquery', 'backbone',
               "change input" : "updateModel",
               "focusout" : "showValidation",
               "keyup .error" : "hideValidation",
-              "click .radio" : "hideValidation",
-              "click .remove-annotation" : "removeAnnotation",
-              "change .eml-annotation input": "updateAnnotations"
+              "click .radio" : "hideValidation"
             },
 
             initialize: function(options){
@@ -217,140 +213,7 @@ define(['underscore', 'jquery', 'backbone',
             	input.removeClass("error");
 
             	this.$("[data-category='" + category + "'] .notification").removeClass("error").empty();
-            },
-
-          /**
-           * addAnnotation
-           *
-           * Adds a new attribute-level annotation to the view and the backing EML
-           * model.
-           */
-          addAnnotation: function (e) {
-            console.log("EMLAttributeView.addAnnotation");
-
-            var annotationListEl = this.$(".attribute-annotation-list");
-
-            if (annotationListEl.length !== 1) {
-              return;
             }
-
-            var model = new EMLAnnotation();
-            var view = new EMLAnnotationView({
-              model: model,
-              isNew: true
-            });
-
-            // Store a reference for handling valiation UI later on
-            this.annotationViews.push(view);
-
-            view.render();
-            annotationListEl.append(view.el);
-
-            this.model.get("annotation").push(model);
-
-            // Ensure the entity has an `id` because annotations require it
-            if (!this.model.get("xmlID")) {
-              this.model.createID();
-            }
-
-            // Update
-            this.model.trickleUpChange();
-          },
-
-          /**
-           * removeAnnotation
-           *
-           * Click event handler that removes the view and model corresponding
-           * to the annotation the user clicked.
-           *
-           * @param {MouseEvent} e - Event handler
-           */
-          removeAnnotation: function (e) {
-            // Don't allow removal of the last view
-            if ($(".attribute-annotation-list .eml-annotation").length <= 1) {
-              return;
-            }
-
-            var annoEl = $(e.target).parents(".eml-annotation").first();
-
-            if (annoEl.length === 0) {
-              return;
-            }
-
-            var index = $(".attribute-annotation-list .eml-annotation").index(annoEl);
-
-            if (index < 0) {
-              return;
-            }
-
-            // Remove view
-            var annotationViews = $(".attribute-annotation-list .eml-annotation");
-
-            if (annotationViews.length < index) {
-              return;
-            }
-
-            // Don't remove view if it's a new view
-            if ($(annotationViews[index]).hasClass("new")) {
-              return;
-            }
-
-            annotationViews[index].remove();
-
-            // Remove from view references
-            if (this.annotationViews.length >= index) {
-              this.annotationViews.splice(index, 1);
-            }
-
-            // Remove model
-            var current = this.model.get("annotation");
-            current.splice(index, 1);
-            this.model.set("annotation", current);
-
-            // Update
-            this.model.trickleUpChange();
-          },
-
-          /**
-           * updateAnnotations
-           *
-           * Event handler responsible for managing the 'new' class on
-           * AnnotationViews which is used to decide when to add a new, blank
-           * view.
-           *
-           * @param {Event} e - Event handler
-           */
-          updateAnnotations: function (e) {
-            console.log("EMLAttributeView.updateAnnotations");
-
-            var annoEl = $(e.target).parents(".eml-annotation").first();
-
-            if (annoEl.length === 0) {
-              return;
-            }
-
-            var index = $(".attribute-annotation-list .eml-annotation").index(annoEl);
-
-            if (index < 0) {
-              return;
-            }
-
-            // Find the view that triggered the event
-            var annotationViews = $(".attribute-annotation-list .eml-annotation");
-
-            if (annotationViews.length < index) {
-              return;
-            }
-
-            // Stop now if we aren't updating an annotation that was previously
-            // new (blank)
-            if (!$(annotationViews[index]).hasClass("new")) {
-              return;
-            }
-
-            $(annotationViews[index]).removeClass("new");
-            this.addAnnotation();
-          },
         });
 
         return EMLAttributeView;
