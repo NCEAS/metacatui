5c66108fa7591e48cedbcd5d7a2c058d13bea806
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2021-02-17 13:12:34 -0500

    Fix bug with creating user permission queries in the Query Builder
    The component required for these inputs was not loading. Now it loads as required.

--- src/js/views/searchSelect/SearchableSelectView.js
+++ src/js/views/searchSelect/SearchableSelectView.js
@@ -7,7 +7,6 @@ define([
     "semanticUIdropdown",
     "text!" + MetacatUI.root + "/components/semanticUI/dropdown.min.css",
     "text!templates/selectUI/searchableSelect.html",
-    
   ],
   function($, _, Backbone, Transition, TransitionCSS, Dropdown, DropdownCSS, Template) {
 
@@ -286,6 +285,14 @@ define([
           try {
 
             var view = this;
+
+            if(view.apiSettings && !view.semanticAPILoaded){
+              require([MetacatUI.root + "/components/semanticUI/api.min.js"], function(SemanticAPI){
+                view.semanticAPILoaded = true
+                view.render();
+              })
+              return;
+            }
 
             // Render the template using the view attributes
             this.$el.html(this.template(this));
