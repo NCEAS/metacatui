de297327daf1793677176f2ec83b2e6fb0c23c60
Bryce <petridish@gmail.com>
2021-05-21 11:43:03 -0800

    Add a fallback description to MetadataView JSON-LD
    Closes https://github.com/NCEAS/metacatui/issues/1803

--- src/js/views/MetadataView.js
+++ src/js/views/MetadataView.js
@@ -2758,6 +2758,9 @@ define(['jquery',
           // Dataset/description
           if (model.get("abstract")) {
             elJSON.description = model.get("abstract");
+          } else {
+            var datasets_url = "https://dataone.org/datasets/" + encodeURIComponent(model.get("id"));
+            elJSON.description = 'No description is available. Visit <a href="' + datasets_url + '">' + datasets_url + '</a> for complete metadata about this dataset.';
           }
 
           // Dataset/keywords
