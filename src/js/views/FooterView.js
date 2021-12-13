21a76b94e86885719e5770de962cd9657a8886cb
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2021-12-13 13:56:03 -0500

    Create FooterView.hide() function
    - Call the function from the Portal View
    - Fix whitespace in the FooterView
    
    Relates to #1924

--- src/js/views/FooterView.js
+++ src/js/views/FooterView.js
@@ -1,23 +1,38 @@
 /*global define */
-define(['jquery', 'underscore', 'backbone', 'text!templates/footer.html'], 				
-	function($, _, Backbone, FooterTemplate) {
-	'use strict';
-	
-	// Build the Footer view of the application
-	var FooterView = Backbone.View.extend({
+define(['jquery', 'underscore', 'backbone', 'text!templates/footer.html'],
+  function ($, _, Backbone, FooterTemplate) {
+    'use strict';
 
-		el: '#Footer',
-		
-		template: _.template(FooterTemplate),
-		
-		initialize: function () {
-		},
-				
-		render: function () {
-			this.$el.html(this.template());
-			
-		}	
-				
-	});
-	return FooterView;		
-});
+    // Build the Footer view of the application
+    var FooterView = Backbone.View.extend({
+
+      el: '#Footer',
+
+      template: _.template(FooterTemplate),
+
+      initialize: function () {
+      },
+
+      render: function () {
+        this.$el.html(this.template());
+      },
+
+      /**
+       * Hide the footer
+       */
+      hide: function () {
+        this.el.style.setProperty('display', 'none') 
+        document.body.style.setProperty('--footer-height', '0')
+      },
+
+      /**
+       * Show the footer after it was hidden
+       */
+      show: function () {
+        this.el.style.removeProperty('display')
+        document.body.style.removeProperty('--footer-height')
+      }
+
+    });
+    return FooterView;
+  });
