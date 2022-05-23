5a0258efd5430308ce210e57bd4b9a274df6c8ab
laurenwalker <walker@nceas.ucsb.edu>
2022-05-23 18:26:29 -0400

    Add first pass at automating Mocha tests in Node #1966 #1961

--- test/server.js
+++ test/server.js
@@ -7,6 +7,8 @@ You'll need node.js and express.js to run this.
 
 See README.md for more details.
 */
+const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
 
 const express = require("express");
 const path = require("path");
@@ -23,7 +25,7 @@ var testType = process.argv.slice(2),
 
 //Append test type argument to the URL. This is only used as a hint when printing to console and routes to root will automatically redirect 
 // to that test type. Any test type can still be executed by passing the `type` URL search parameter.
-if( testType && testType.length ){
+if( testType && testType!="keep-running" && testType.length ){
     url += "?type=" + testType
 }
 
@@ -31,6 +33,47 @@ if( testType && testType.length ){
 app.get('/', (req, res) => {
     res.redirect(url);
 })
+//Start the server
+const server = app.listen(port);
 
-app.listen(port);
-console.log("Tests are now running at http://localhost:" + port + url);
+//Get the full URL where tests are
+url="http://localhost:"+port+url;
+
+async function ssr(url) {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.goto(url, {waitUntil: 'networkidle0'});
+  const html = await page.content(); // serialized HTML of page DOM.
+  await browser.close();
+  //console.log(html)
+//  const result_html = ssr("http://localhost:"+port+url);
+const $ = cheerio.load(html);
+
+//Get and print the results
+let passes=parseInt($("#mocha-stats .passes em").text()) || 0;
+let fails=parseInt($("#mocha-stats .failures em").text()) || 0;
+
+console.log(`PASSES: ${passes}`);
+
+if(fails>0){
+    console.error(`FAILS: ${fails}`);
+    console.error(`Test failure details can be viewed by running "npm view-tests" and visiting ${url}`)
+}
+else{
+    console.error(`FAILS: ${fails}`);
+}
+
+  if(testType!="keep-running"){
+    server.close();
+  }
+  else{
+      console.log(`Test results are available at ${url}`);
+  }
+
+  return html;
+}
+
+ssr(url)
+
+
+
