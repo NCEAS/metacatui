aed57940b38887eed29b0370dbd87a4159564c15
Robyn Thiessen-Bock <robyn.thiessenbock@gmail.com>
2023-05-05 15:58:16 -0400

    Standardize formatting in BooleanFilterView

--- src/js/views/filters/BooleanFilterView.js
+++ src/js/views/filters/BooleanFilterView.js
@@ -1,79 +1,75 @@
 /*global define */
-define(['jquery', 'underscore', 'backbone',
-        'models/filters/BooleanFilter',
-        'views/filters/FilterView',
-        'text!templates/filters/booleanFilter.html'],
-  function($, _, Backbone, BooleanFilter, FilterView, Template) {
-  'use strict';
+define([
+  "jquery",
+  "underscore",
+  "backbone",
+  "models/filters/BooleanFilter",
+  "views/filters/FilterView",
+  "text!templates/filters/booleanFilter.html",
+], function ($, _, Backbone, BooleanFilter, FilterView, Template) {
+  "use strict";
 
   /**
-  * @class BooleanFilterView
-  * @classdesc Render a view of a single BooleanFilter model
-  * @classcategory Views/Filters
-  */
+   * @class BooleanFilterView
+   * @classdesc Render a view of a single BooleanFilter model
+   * @classcategory Views/Filters
+   */
   var BooleanFilterView = FilterView.extend(
-    /** @lends BooleanFilterView.prototype */{
+    /** @lends BooleanFilterView.prototype */ {
+      /**
+       * A BooleanFilter model to be rendered in this view
+       * @type {BooleanFilter} */
+      model: null,
 
-    /**
-    * A BooleanFilter model to be rendered in this view
-    * @type {BooleanFilter} */
-    model: null,
+      className: "filter boolean",
 
-    className: "filter boolean",
+      template: _.template(Template),
 
-    template: _.template(Template),
+      events: {
+        "click input[type='checkbox']": "updateModel",
+      },
 
-    events: {
-      "click input[type='checkbox']" : "updateModel"
-    },
+      initialize: function (options) {
+        if (!options || typeof options != "object") {
+          var options = {};
+        }
 
-    initialize: function (options) {
+        this.model = options.model || new BooleanFilter();
+      },
 
-      if( !options || typeof options != "object" ){
-        var options = {};
-      }
+      render: function () {
+        this.$el.html(this.template(this.model.toJSON()));
 
-      this.model = options.model || new BooleanFilter();
+        this.listenTo(this.model, "change:values", this.updateCheckbox);
+      },
 
-    },
+      /**
+       * Gets the value of the checkbox and updates the BooleanFilter model
+       */
+      updateModel: function () {
+        //Find out if the checkbox has been checked or not
+        var isChecked = this.$("input[type='checkbox']").prop("checked");
 
-    render: function () {
-      this.$el.html( this.template( this.model.toJSON() ) );
+        //Set the boolean value on the model
+        this.model.set("values", [isChecked]);
+      },
 
-      this.listenTo( this.model, "change:values", this.updateCheckbox );
-    },
+      /**
+       * Updates the checked property of the checkbox based on the model value
+       */
+      updateCheckbox: function () {
+        //Get the value from the model
+        var modelValue = this.model.get("values")[0];
 
-    /**
-    * Gets the value of the checkbox and updates the BooleanFilter model
-    */
-    updateModel: function(){
+        //If the model value is falsey, then set to false
+        if (!modelValue) {
+          modelValue = false;
+        }
 
-      //Find out if the checkbox has been checked or not
-      var isChecked = this.$("input[type='checkbox']").prop("checked");
-
-      //Set the boolean value on the model
-      this.model.set("values", [isChecked]);
-
-    },
-
-    /**
-    * Updates the checked property of the checkbox based on the model value
-    */
-    updateCheckbox: function(){
-
-      //Get the value from the model
-      var modelValue = this.model.get("values")[0];
-
-      //If the model value is falsey, then set to false
-      if( !modelValue ){
-        modelValue = false;
-      }
-
-      //Update the checkbox based on the model value
-      this.$("input[type='checkbox']").prop("checked", modelValue);
-
+        //Update the checkbox based on the model value
+        this.$("input[type='checkbox']").prop("checked", modelValue);
+      },
     }
-
-  });
+  );
   return BooleanFilterView;
 });
