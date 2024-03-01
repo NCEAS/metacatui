784b5317edf2c243d0202f509a1091802527d815
ianguerin <ianguerin@gmail.com>
2024-02-29 17:08:00 -0800

    Create child views managing list of Predictions
    Add ViewfinderModel which will be used by ViewfinderView in a later
    commit.
    Prediction models can be represented by a PredictionView which are in
    turn managed as an unordered list.

--- src/js/models/geocoder/Prediction.js
+++ src/js/models/geocoder/Prediction.js
@@ -9,16 +9,16 @@ define(['backbone'], (Backbone) => {
   * @since x.x.x
   */
   const Prediction = Backbone.Model.extend({
-      /**
-       * Overrides the default Backbone.Model.defaults() function to specify
-       * default attributes for the Map
-       * @name Prediction#defaults
-       * @type {Object}
-       * @property {string} description A user-friendly description of a Google
-       * Maps Place.
-       * @property {string} googleMapsPlaceId Unique identifier that can be 
-       * geocoded by the Google Maps Geocoder API.
-       */
+    /**
+     * Overrides the default Backbone.Model.defaults() function to specify
+     * default attributes for the Map
+     * @name Prediction#defaults
+     * @type {Object}
+     * @property {string} description A user-friendly description of a Google
+     * Maps Place.
+     * @property {string} googleMapsPlaceId Unique identifier that can be 
+     * geocoded by the Google Maps Geocoder API.
+     */
     defaults() {
       return { description: '', googleMapsPlaceId: '' };
     },
