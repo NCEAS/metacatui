08838e3d42ab8e808c84acf268c4e11e04357220
ianguerin <ianguerin@gmail.com>
2024-02-29 17:08:00 -0800

    Create child views managing list of Predictions
    Add ViewfinderModel which will be used by ViewfinderView in a later
    commit.
    Prediction models can be represented by a PredictionView which are in
    turn managed as an unordered list.

---
+++ test/js/specs/unit/views/maps/viewfinder/PredictionViewHarness.js
@@ -0,0 +1,21 @@
+"use strict";
+
+define([], function () {
+  return class PredictionViewHarness {
+    constructor(view) {
+      this.view = view;
+    }
+
+    click() {
+      this.view.$el.click();
+    }
+
+    isFocused() {
+      return this.view.$el.find('.viewfinder-prediction__focused').length === 1;
+    }
+
+    getDescription() {
+      return this.view.$el.find('.viewfinder-prediction__main').text();
+    }
+  }
+});
