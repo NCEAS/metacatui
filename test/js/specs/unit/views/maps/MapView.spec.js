adf9e74168f8793696f73a968ddfc534aba4cef6
yvonnesjy <yvonneshi@google.com>
2024-04-17 13:15:12 -0700

    Fixup: Add tests

---
+++ test/js/specs/unit/views/maps/MapView.spec.js
@@ -0,0 +1,25 @@
+define([
+  "views/maps/MapView",
+  'models/maps/Map',
+], (MapView, MapAsset) => {
+  const expect = chai.expect;
+
+  describe("MapView Test Suite", () => {
+    describe("Initialization", () => {
+      it("creates a MapView instance", () => {
+        const view = new MapView();
+        expect(view).to.be.instanceof(MapView);
+      });
+    });
+
+    describe("Portal map", () => {
+      it("has an additional portal indicator class", () => {
+        const nonPortalMap = new MapView();
+        expect(nonPortalMap.$el.hasClass(nonPortalMap.classes.portalIndicator)).to.be.false;
+        
+        const portalMap = new MapView({ isPortalMap: true });
+        expect(portalMap.$el.hasClass(portalMap.classes.portalIndicator)).to.be.true;
+      });
+    });
+  });
+});
