515cf281ec27df3316097b1c3f54283022c4996d
ianguerin <ianguerin@gmail.com>
2024-04-17 10:51:08 -0700

    Fixup: Docs, screenshots, and class names

--- src/js/models/maps/viewfinder/ExpansionPanelsModel.js
+++ src/js/models/maps/viewfinder/ExpansionPanelsModel.js
@@ -3,57 +3,52 @@
 define([], () => {
   /**
   * @class ExpansionPanelsModel
-  * @classdes ExpansionPanelsModel maintains state for multiple
+  * @classdesc ExpansionPanelsModel maintains state for multiple
   * ExpansionPanelView instances so that only one is open at a time.
   * @classcategory Models/Maps
   */
-  const ExpansionPanelsModel = Backbone.Model.extend({
-    /**
-     * @name ExpansionPanelsModel#defaults
-     * @type {Object}
-     * @property {ExpansionPanelView[]} panels The expansion panel views that
-     * are meant to have only a single panel open at a time.
-     * @property {boolean} isMulti Whether multiple panels can be open at the
-     * same time when displayed in a group of panels. 
-     */
-    defaults() {
-      return {
-        panels: [],
-        isMulti: false,
-      }
-    },
+  const ExpansionPanelsModel = Backbone.Model.extend(
+    /** @lends ExpansionPanelsModel.prototype */{
+      /**
+       * @name ExpansionPanelsModel#defaults
+       * @type {Object}
+       * @property {ExpansionPanelView[]} panels The expansion panel views that
+       * are meant to have only a single panel open at a time.
+       * @property {boolean} isMulti Whether multiple panels can be open at the
+       * same time when displayed in a group of panels. 
+       * @extends Backbone.Model 
+       */
+      defaults() {
+        return {
+          panels: [],
+          isMulti: false,
+        }
+      },
 
-    /**
-     * @param {boolean} isMulti The display mode of the panels.
-     */
-    initialize({ isMulti } = { isMulti: false }) {
-      this.set('isMulti', isMulti);
-    },
+      /**
+       * Register a panel to coordinate collapse state.
+       * @property {ExpansionPanelView} panel The expansion panel view to be 
+       * tracked.
+       */
+      register(panel) {
+        this.set('panels', [...this.get('panels'), panel]);
+      },
 
-    /**
-     * Register a panel to coordinate collapse state.
-     * @property {ExpansionPanelView} panel The expansion panel view to be 
-     * tracked.
-     */
-    register(panel) {
-      this.set('panels', [...this.get('panels'), panel]);
-    },
-
-    /**
-     * Collapse all panels except for the newly opened panel for certain open
-     * modes.
-     * @property {ExpansionPanelView} openedPanel The expansion panel view that 
-     * should remain open.
-     */
-    collapseOthers(openedPanel) {
-      const isSingleOpenMode = !this.get('isMulti');
-      for (const panel of this.get('panels')) {
-        if (isSingleOpenMode && panel !== openedPanel) {
-          panel.collapse();
+      /**
+       * Collapse all panels except for the newly opened panel for certain open
+       * modes.
+       * @property {ExpansionPanelView} openedPanel The expansion panel view that 
+       * should remain open.
+       */
+      collapseOthers(openedPanel) {
+        const isSingleOpenMode = !this.get('isMulti');
+        for (const panel of this.get('panels')) {
+          if (isSingleOpenMode && panel !== openedPanel) {
+            panel.collapse();
+          }
         }
       }
-    }
-  });
+    });
 
   return ExpansionPanelsModel;
 });
--- src/js/views/maps/viewfinder/ExpansionPanelView.js
+++ src/js/views/maps/viewfinder/ExpansionPanelView.js
@@ -9,6 +9,13 @@ define(
   (_, Backbone, Template) => {
     // The base classname to use for this View's template elements.
     const BASE_CLASS = 'expansion-panel';
+    //The HTML classes to use for this view's HTML elements.
+    const CLASS_NAMES = {
+      title: `${BASE_CLASS}__title`,
+      content: `${BASE_CLASS}__content`,
+      toggle: `${BASE_CLASS}__toggle`,
+      icon: `${BASE_CLASS}__icon`,
+    };
 
     /**
      * @class ExpansionPanelView
@@ -21,107 +28,93 @@ define(
      * @since x.x.x
      * @constructs ExpansionPanelView
      */
-    var ExpansionPanelView = Backbone.View.extend({
-      /**
-       * The type of View this is
-       * @type {string}
-       */
-      type: 'ExpansionPanelView',
+    var ExpansionPanelView = Backbone.View.extend(
+      /** @lends ExpansionPanelView.prototype */{
+        /**
+         * The type of View this is
+         * @type {string}
+         */
+        type: 'ExpansionPanelView',
 
-      /**
-       * The HTML class to use for this view's outermost element.
-       * @type {string}
-       */
-      className: BASE_CLASS,
+        /** @inheritdoc */
+        className: BASE_CLASS,
 
-      /**
-       * The HTML classes to use for this view's HTML elements.
-       * @type {Object<string,string>}
-       */
-      classNames: {
-        title: `${BASE_CLASS}__title`,
-        content: `${BASE_CLASS}__content`,
-        toggle: `${BASE_CLASS}__toggle`,
-        icon: `${BASE_CLASS}__icon`,
-      },
+        /**
+        * The events this view will listen to and the associated function to call.
+        * @type {Object}
+        */
+        events() {
+          return {
+            [`click .${CLASS_NAMES.toggle}`]: 'toggle',
+          };
+        },
 
-      /**
-      * The events this view will listen to and the associated function to call.
-      * @type {Object}
-      */
-      events() {
-        return {
-          [`click .${this.classNames.toggle}`]: 'toggle',
-        };
-      },
+        /** Values meant to be used by the rendered HTML template. */
+        templateVars: {
+          classNames: CLASS_NAMES,
+          title: '',
+          icon: '',
+        },
 
-      /** Values meant to be used by the rendered HTML template. */
-      templateVars: {
-        classNames: {},
-        title: '',
-        icon: '',
-      },
+        /**
+         * @typedef {Object} ExpansionPanelViewOptions
+         * @property {string} title The displayed label for this panel. 
+         * @property {string} icon The icon displayed in the panel's clickable
+         * label. 
+         * @property {Backbone.View} contentViewInstance The Backbone.View that 
+         * will be displayed when the content of the panel is toggled to be
+         * visible.
+         * @property {ExpansionPanelsModel} [panelsModel] Optional model for
+         * coordinating the expanded/collapsed state among many panels. 
+         */
+        initialize({ title, contentViewInstance, icon, panelsModel }) {
+          this.templateVars.title = title;
+          this.templateVars.icon = icon;
+          this.contentViewInstance = contentViewInstance;
+          this.panelsModel = panelsModel;
 
-      /**
-       * @typedef {Object} ExpansionPanelViewOptions
-       * @property {string} title The displayed label for this panel. 
-       * @property {string} icon The icon displayed in the panel's clickable
-       * label. 
-       * @property {Backbone.View} contentViewInstance The Backbone.View that 
-       * will be displayed when the content of the panel is toggled to be
-       * visible.
-       * @property {ExpansionPanelsModel} [panelsModel] Optional model for
-       * coordinating the expanded/collapsed state among many panels. 
-       */
-      initialize({ title, contentViewInstance, icon, panelsModel }) {
-        this.templateVars.classNames = this.classNames;
-        this.templateVars.title = title;
-        this.templateVars.icon = icon;
-        this.contentViewInstance = contentViewInstance;
-        this.panelsModel = panelsModel;
+          this.panelsModel?.register(this);
+        },
 
-        this.panelsModel?.register(this);
-      },
+        /**
+         * Getter function for the content div. 
+         * @return {HTMLDivElement} Returns the content element.
+         */
+        getContent() {
+          return this.$el.find(`.${CLASS_NAMES.content}`);
+        },
 
-      /**
-       * Getter function for the content div. 
-       * @return {HTMLDivElement} Returns the content element.
-       */
-      getContent() {
-        return this.$el.find(`.${this.classNames.content}`);
-      },
+        /** Force the panel's content to be hidden. */
+        collapse() {
+          this.$el.removeClass('show-content');
+        },
 
-      /** Force the panel's content to be hidden. */
-      collapse() {
-        this.$el.removeClass('show-content');
-      },
+        /** Force the panel's content to be shown. */
+        open() {
+          this.$el.addClass('show-content');
+        },
 
-      /** Force the panel's content to be shown. */
-      open() {
-        this.$el.addClass('show-content');
-      },
+        /** Toggle the visibility of the panel's content. */
+        toggle() {
+          if (this.$el.hasClass('show-content')) {
+            this.collapse();
+          } else {
+            this.open();
+            this.panelsModel?.collapseOthers(this);
+          }
+        },
 
-      /** Toggle the visibility of the panel's content. */
-      toggle() {
-        if (this.$el.hasClass('show-content')) {
-          this.collapse();
-        } else {
-          this.open();
-          this.panelsModel?.collapseOthers(this);
-        }
-      },
-
-      /**
-       * Render the view by updating the HTML of the element.
-       * The new HTML is computed from an HTML template that
-       * is passed an object with relevant view state.
-       * */
-      render() {
-        this.el.innerHTML = _.template(Template)(this.templateVars);
-        this.contentViewInstance.render();
-        this.getContent().append(this.contentViewInstance.el);
-      },
-    });
+        /**
+         * Render the view by updating the HTML of the element.
+         * The new HTML is computed from an HTML template that
+         * is passed an object with relevant view state.
+         * */
+        render() {
+          this.el.innerHTML = _.template(Template)(this.templateVars);
+          this.contentViewInstance.render();
+          this.getContent().append(this.contentViewInstance.el);
+        },
+      });
 
     return ExpansionPanelView;
   });
