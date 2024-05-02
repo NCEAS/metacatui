2932abb41a0814fa10c341f668fc95399aa96218
Ian Guerin <ianguerin@gmail.com>
2024-05-02 09:14:57 -0700

    Update src/js/models/maps/Map.js
    Co-authored-by: Robyn <26600641+robyngit@users.noreply.github.com>

--- src/js/models/maps/Map.js
+++ src/js/models/maps/Map.js
@@ -402,6 +402,7 @@ define([
        * flat list of MapAsset models.
        * @returns {MapAsset[]} All of the layers, or empty array if no layers
        * are configured.
+       * @since x.x.x
        */
       getAllLayers() {
         if (this.has("layerCategories")) {
