56b68b1d63fade0ff78063cb4b8457f2e98f890e
robyngit <thiessenbock@nceas.ucsb.edu>
2024-05-21 13:19:05 -0400

    Add code with linting and formatting errors

--- src/js/views/TOCView.js
+++ src/js/views/TOCView.js
@@ -468,6 +468,14 @@ define([
         $("body").off("activate");
         $(window).off("resize");
       },
+
+      tempMethodWithLintingFormattingErrors: function() {
+      const a = 1;
+      a = 2;
+           b = 99;
+         console.log("This is a test of the linting errors.");
+           
+      }
     },
   );
 
