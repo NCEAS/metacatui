edda8943614874f39d224a2ed2eb8817aba253b2
Matthew B <106352182+artntek@users.noreply.github.com>
2024-06-03 15:25:08 -0700

    add configmap for config.js; git checkout static content

---
+++ helm/config/config.js
@@ -0,0 +1,28 @@
+MetacatUI.AppConfig = {
+    root: {{ .Values.appConfig.root | quote }},
+    theme: {{ .Values.appConfig.theme | quote }},
+    baseUrl: {{ .Values.appConfig.baseUrl | quote }}
+    {{- $optionalStringValues := list
+        "d1CNBaseUrl"
+        "mapKey"
+        "mdqBaseUrl"
+        "dataoneSearchUrl"
+        "googleAnalyticsKey"
+        "bioportalAPIKey"
+        "cesiumToken"
+    -}}
+    {{- $optionalIntValues := list
+        "portalLimit"
+    -}}
+    {{- range $key, $value := .Values.appConfig }}
+        {{- if has $key $optionalStringValues }}
+            {{- (printf ",") }}
+            {{- (printf "%s: \"%s\"" $key (toString $value)) | nindent 6 }}
+        {{- else }}
+            {{- if has $key $optionalIntValues }}
+                {{- (printf ",") }}
+                {{- (printf "%s: %s" $key (toString $value)) | nindent 6 }}
+            {{- end }}
+        {{- end }}
+    {{- end }}
+}
