b72482c701e9239999a2635ec3c8b271de3bd196
robyngit <thiessenbock@nceas.ucsb.edu>
2024-08-06 13:58:51 -0400

    Add simple searchSelect solr autocomplete
    Relates to #1654, #2035, #2363

---
+++ src/js/views/searchSelect/SolrAutocompleteView.js
@@ -0,0 +1,36 @@
+define([
+  "underscore",
+  "views/searchSelect/SearchSelectView",
+  "models/searchSelect/SolrAutocomplete",
+], (_, SearchSelect, SolrAutocomplete) => {
+
+  /**
+   * @class
+   * @classdesc
+   * @classcategory Views/SearchSelect
+   * @augments SearchSelect
+   * @class
+   * @since 0.0.0
+   * @screenshot
+   */
+  const SolrAutocompleteView = SearchSelect.extend(
+    /** @lends SolrAutocompleteView.prototype */
+    {
+      /** @inheritdoc */
+      type: "SolrAutocomplete",
+
+      /** @inheritdoc */
+      className: `${SearchSelect.prototype.className} solr-autocomplete`,
+
+      /** @inheritdoc */
+      ModelType: SolrAutocomplete,
+
+      /**
+       * The name of the field in the Solr schema that the user is searching.
+       * @type {string}
+       */
+      queryField: "text",
+    },
+  );
+  return SolrAutocompleteView;
+});
