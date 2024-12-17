dae8989595efc14c84f1338ff40a3f3526580b7e
robyngit <thiessenbock@nceas.ucsb.edu>
2024-12-17 16:22:14 -0500

    Serialize shortname in EML211
    Added shortname to the array of basic text elements
    
    Fixes #2536

--- src/js/models/metadata/eml211/EML211.js
+++ src/js/models/metadata/eml211/EML211.js
@@ -775,8 +775,8 @@ define([
 
         var nodeNameMap = this.nodeNameMap();
 
-        //Serialize the basic text fields
-        var basicText = ["alternateIdentifier", "title"];
+        // Serialize the basic text fields
+        const basicText = ["alternateIdentifier", "title", "shortName"];
         _.each(
           basicText,
           function (fieldName) {
