d3243e0d2a2e0cb31edd86444423454adda24a68
robyngit <thiessenbock@nceas.ucsb.edu>
2025-11-13 14:56:39 -0500

    Update ORCID base URL to v3 api endpoint
    Issue #2762

--- src/js/models/LookupModel.js
+++ src/js/models/LookupModel.js
@@ -264,41 +264,8 @@ define(["jquery", "jqueryui", "underscore", "backbone"], function (
         });
       },
 
-      orcidGetConcepts: function (uri, callback) {
-        var people = this.get("concepts")[uri];
-
-        if (people) {
-          callback(people);
-          return;
-        } else {
-          people = [];
-        }
-
-        var query =
-          MetacatUI.appModel.get("orcidBaseUrl") +
-          uri.substring(uri.lastIndexOf("/"));
-        var model = this;
-        $.get(query, function (data, status, xhr) {
-          // get the orcid info
-          var profile = $(data).find("orcid-profile");
-
-          _.each(profile, function (obj) {
-            var choice = {};
-            choice.label =
-              $(obj).find("orcid-bio > personal-details > given-names").text() +
-              " " +
-              $(obj).find("orcid-bio > personal-details > family-name").text();
-            choice.value = $(obj).find("orcid-identifier > uri").text();
-            choice.desc = $(obj).find("orcid-bio > personal-details").text();
-            people.push(choice);
-          });
-
-          model.get("concepts")[uri] = people;
-
-          // callback with answers
-          callback(people);
-        });
-      },
+      /** @deprecated since 0.0.0 */
+      orcidGetConcepts: function () {},
 
       /*
        * Supplies search results for ORCiDs to autocomplete UI elements
