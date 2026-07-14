ee88d0f8eaf33146f54a69ec1e7733b32b786a6f
Darian Gill <dgill@nceas.ucsb.edu>
2026-07-14 11:25:24 -1000

    Updated imagery test fixture in CesiumWidgetView.spec.js to set visible: true matching new visibility semantics
    Issue: #2635

--- test/js/specs/unit/views/maps/CesiumWidgetView.spec.js
+++ test/js/specs/unit/views/maps/CesiumWidgetView.spec.js
@@ -77,6 +77,7 @@ define([
         return {
           label: `layer ${type}`,
           type,
+          visible: true,
           cesiumOptions: {
             url: "https://example.com",
           },
